3799304a830b91200f17cfb73d813359a3965812
Marco Metz <marco.metz@gmail.com>
2012-01-18 18:35:40 +0100

    restrukture templates

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,17 +1,17 @@
-  class User < ActiveRecord::Base
-    # Include default devise modules. Others available are:
-    # :token_authenticatable, :encryptable, :confirmable, :lockable, :timeoutable and :omniauthable, 
-    devise :database_authenticatable, :recoverable, :rememberable, :trackable, :validatable, :lockable, :omniauthable, :registerable, :confirmable
-  
-    # Setup accessible (or protected) attributes for your model
-    attr_accessible :email, :password, :password_confirmation, :remember_me
-    has_and_belongs_to_many :roles, :join_table => "goldencobra_roles_users", :class_name => Goldencobra::Role
-  
-    def has_role?(name)
-      self.roles.include?(Role.find_by_name("admin"))
-    end
-  
+class User < ActiveRecord::Base
+  # Include default devise modules. Others available are:
+  # :token_authenticatable, :encryptable, :confirmable, :lockable, :timeoutable and :omniauthable, 
+  devise :database_authenticatable, :recoverable, :rememberable, :trackable, :validatable, :lockable, :omniauthable, :registerable, :confirmable
+
+  # Setup accessible (or protected) attributes for your model
+  attr_accessible :email, :password, :password_confirmation, :remember_me
+  has_and_belongs_to_many :roles, :join_table => "goldencobra_roles_users", :class_name => Goldencobra::Role
+
+  def has_role?(name)
+    self.roles.include?(Goldencobra::Role.find_by_name("admin"))
   end
+
+end
 # == Schema Information
 #
 # Table name: users
