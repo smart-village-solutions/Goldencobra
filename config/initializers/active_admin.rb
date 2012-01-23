9d84014ab3ca9cb887965aa772b539eee5c0a02c
Marco Metz <marco.metz@gmail.com>
2012-01-23 17:49:53 +0100

    modified:   ../../config/initializers/active_admin.rb

--- config/initializers/active_admin.rb
+++ config/initializers/active_admin.rb
@@ -1,3 +1,8 @@
+require "activeadmin"
+#require "friendly_id"
+#require 'ancestry'
+#require 'devise'
+
 ActiveAdmin.setup do |config|
   if ActiveAdmin::VERSION == "0.3.4" && Rails.version == "3.2.0.rc2"
     class ActiveSupport::FileUpdateChecker
