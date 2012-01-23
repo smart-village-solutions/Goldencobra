eed9355d23716e267feb2f9e4e83b89db0101458
Marco Metz <marco.metz@gmail.com>
2012-01-23 18:28:19 +0100

    modified:   ../../README.rdoc 	modified:   ../../app/controllers/goldencobra/articles_controller.rb 	modified:   ../../lib/goldencobra/version.rb

--- app/controllers/goldencobra/articles_controller.rb
+++ app/controllers/goldencobra/articles_controller.rb
@@ -7,7 +7,8 @@ module Goldencobra
       else
         @article = Article.find(params[:article_id].split("/").last)
       end
-      set_meta_tags :site => "Goldencobra",
+      if @article
+        set_meta_tags :site => "Goldencobra",
                     :title => @article.metatag("Title Tag"),
                     :description => @article.metatag("Meta Description"), 
                     :keywords => @article.metatag("Keywords"),
@@ -15,7 +16,8 @@ module Goldencobra
                                     :type => @article.metatag("OpenGraph Type"),
                                     :url => @article.metatag("OpenGraph URL"), 
                                     :image => @article.metatag("OpenGraph Image")}
-
+      end
+      
     end
   end
 end
