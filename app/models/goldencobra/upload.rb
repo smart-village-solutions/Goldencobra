a01c3b3820ed8cb207a292bca7949b1e7edfc487
Marco Metz <marco.metz@gmail.com>
2012-01-30 16:54:09 +0100

    modified:   ../../app/models/goldencobra/upload.rb

--- app/models/goldencobra/upload.rb
+++ app/models/goldencobra/upload.rb
@@ -1,6 +1,6 @@
 module Goldencobra
   class Upload < ActiveRecord::Base
-    if Goldencobra::Upload.table_exists?("goldencobra_uploads")
+    if ActiveRecord::Base.connection.table_exists?("goldencobra_uploads")
       has_attached_file :image, :styles => { :large => "900x900>",:big => "600x600>", :medium => "300x300>", :thumb => "100x100>", :mini => "50x50>" }
     end
   end
