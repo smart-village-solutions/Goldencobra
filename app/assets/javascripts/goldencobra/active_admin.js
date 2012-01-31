77a606bf715bee2d4b109c4fd7847c8b6e6c51f4
Marco Metz <marco.metz@gmail.com>
2012-01-31 13:45:53 +0100

    modified:   app/assets/javascripts/goldencobra/active_admin.js

--- app/assets/javascripts/goldencobra/active_admin.js
+++ app/assets/javascripts/goldencobra/active_admin.js
@@ -4,13 +4,16 @@ $(document).ready(function() {
 		script_url: "/assets/goldencobra/tiny_mce.js",
   		mode : "textareas",
   		theme : "advanced",
-  		theme_advanced_buttons1 : "formatselect, bold, italic, underline, strikethrough, |, bullist, numlist, blockquote, |, undo, redo, |, link, unlink, code",
+  		theme_advanced_buttons1 : "formatselect, bold, italic, underline, strikethrough, |, bullist, numlist, blockquote, |, pastetext,pasteword, |, undo, redo, |, link, unlink, code, fullscreen",
   		theme_advanced_buttons2 : "",
   		theme_advanced_buttons3 : "",
   		theme_advanced_toolbar_location : "top",
   		theme_advanced_toolbar_align : "center",
-  		theme_advanced_resizing : false,
+  		theme_advanced_resizing : false, 
 		relative_urls : true,
-		theme_advanced_blockformats : "p,h1,h2,h3,div"
+		theme_advanced_blockformats : "p,h1,h2,h3,div",
+		plugins : "fullscreen,autolink,inlinepopups,paste",
+		dialog_type : "modal",
+		paste_auto_cleanup_on_paste : true
 	});
 });
