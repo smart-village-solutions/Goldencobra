3dd7ce257abca5fae66dbc87b25d0b66d2a2f92b
Marco Metz <marco.metz@gmail.com>
2012-01-31 13:25:57 +0100

    tyne mcs installed completely

--- app/assets/javascripts/goldencobra/active_admin.js
+++ app/assets/javascripts/goldencobra/active_admin.js
@@ -4,11 +4,13 @@ $(document).ready(function() {
 		script_url: "/assets/goldencobra/tiny_mce.js",
   		mode : "textareas",
   		theme : "advanced",
-  		theme_advanced_buttons1 : "bold, italic, underline, strikethrough, |, bullist, numlist, blockquote, |, undo, redo, |, link, unlink, code",
+  		theme_advanced_buttons1 : "formatselect, bold, italic, underline, strikethrough, |, bullist, numlist, blockquote, |, undo, redo, |, link, unlink, code",
   		theme_advanced_buttons2 : "",
   		theme_advanced_buttons3 : "",
   		theme_advanced_toolbar_location : "top",
   		theme_advanced_toolbar_align : "center",
-  		theme_advanced_resizing : false
+  		theme_advanced_resizing : false,
+		relative_urls : true,
+		theme_advanced_blockformats : "p,h1,h2,h3,div"
 	});
 });
