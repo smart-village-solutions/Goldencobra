9dde544f0497343ce378f658b9ec45bce4133653
Marco Metz <marco.metz@gmail.com>
2012-02-08 16:10:03 +0100

    [#24635575] for all image_drop_down_lists: image.complete_list_name renders full image name, rights and creationdate

--- app/models/goldencobra/upload.rb
+++ app/models/goldencobra/upload.rb
@@ -3,5 +3,12 @@ module Goldencobra
     if ActiveRecord::Base.connection.table_exists?("goldencobra_uploads")
       has_attached_file :image, :styles => { :large => "900x900>",:big => "600x600>", :medium => "300x300>", :thumb => "100x100>", :mini => "50x50>" }
     end
+    
+    def complete_list_name 
+      result = ""
+      result << "#{self.image_file_name} " if self.image_file_name.present?
+      result << "(#{[self.source,self.rights].join(', ')}) " if self.source.present? || self.rights.present?
+      result << "- #{self.created_at}"
+    end
   end
 end
