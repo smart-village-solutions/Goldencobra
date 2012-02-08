364e0dfa8c3600ac0872c014198d701018cabb18
Marco Metz <marco.metz@gmail.com>
2012-02-08 15:10:31 +0100

    Uploader ready

--- app/admin/uploads.rb
+++ app/admin/uploads.rb
@@ -1,5 +1,6 @@
 ActiveAdmin.register Goldencobra::Upload, :as => "Upload"  do
   
+  
   form :html => { :enctype => "multipart/form-data" }  do |f|
     f.inputs "Allgemein" do
       f.input :source
