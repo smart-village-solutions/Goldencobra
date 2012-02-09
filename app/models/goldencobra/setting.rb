87443291ee80968c7a272f4a8eda829dd4597ca1
Marco Metz <marco.metz@gmail.com>
2012-02-09 16:45:43 +0100

    modified:   app/models/goldencobra/setting.rb

--- app/models/goldencobra/setting.rb
+++ app/models/goldencobra/setting.rb
@@ -23,12 +23,14 @@ module Goldencobra
     
     
     def self.import_default_settings(path_file_name)
-      require 'yaml'
-      raise "Settings File '#{path_file_name}' does not exist" if !File.exists?(path_file_name)
-      imports = open(path_file_name) {|f| YAML.load(f) }
-      imports.each_key do |key|
-        generate_default_setting(key, imports)
-      end      
+      if ActiveRecord::Base.connection.table_exists?("goldencobra_settings")
+        require 'yaml'
+        raise "Settings File '#{path_file_name}' does not exist" if !File.exists?(path_file_name)
+        imports = open(path_file_name) {|f| YAML.load(f) }
+        imports.each_key do |key|
+          generate_default_setting(key, imports)
+        end  
+      end    
     end
     
     
