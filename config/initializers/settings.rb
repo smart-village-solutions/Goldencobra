c546975df526d27a4d84aa4de98dcf51279641a3
Marco Metz <marco.metz@gmail.com>
2012-02-15 12:53:55 +0100

    modified:   config/initializers/settings.rb

--- config/initializers/settings.rb
+++ config/initializers/settings.rb
@@ -1,22 +1,35 @@
 Goldencobra::Setting.import_default_settings(Goldencobra::Engine.root + "config/settings.yml")
 
-::ApplicationHelper.module_eval do
-  
-  def s(name)
-    if name.present?
-      Goldencobra::Setting.for_key(name)
-    end
-  end
-  
-end
+#::ApplicationHelper.module_eval do
+#  def s(name)
+#    if name.present?
+#      Goldencobra::Setting.for_key(name)
+#    end
+#  end
+#end
+#
+#
+#::ActionController::Base.module_eval do
+#  def s(name)
+#    if name.present?
+#      Goldencobra::Setting.for_key(name)
+#    end
+#  end
+#end
 
-
-::ActionController::Base.module_eval do
-  
-  def s(name)
-    if name.present?
-      Goldencobra::Setting.for_key(name)
+module Goldencobra
+    module ActsAsSetting
+        module Controller
+          
+            def s(name)
+              if name.present?
+                Goldencobra::Setting.for_key(name)
+              end
+            end
+            
+        end
     end
-  end
-  
 end
+       
+::ActionController::Base.send :include, Goldencobra::ActsAsSetting::Controller
+::ApplicationHelper.send :include, Goldencobra::ActsAsSetting::Controller
