f65ca15a47be7017eaea714704a1279808720525
Marco Metz <marco.metz@gmail.com>
2012-02-15 13:04:29 +0100

    new file:   lib/acts_as_setting.rb

---
+++ lib/acts_as_setting.rb
@@ -0,0 +1,16 @@
+module Goldencobra
+    module ActsAsSetting
+        module Controller
+          
+            def s(name)
+              if name.present?
+                Goldencobra::Setting.for_key(name)
+              end
+            end
+            
+        end
+    end
+end
+       
+::ActionController::Base.send :include, Goldencobra::ActsAsSetting::Controller
+::ApplicationHelper.send :include, Goldencobra::ActsAsSetting::Controller
