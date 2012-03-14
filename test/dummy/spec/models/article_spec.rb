deac18ec2994f90eb83e03e2824c3741e49bd58c
Marco Metz <marco.metz@gmail.com>
2012-03-14 12:34:26 +0100

    template

--- test/dummy/spec/models/article_spec.rb
+++ test/dummy/spec/models/article_spec.rb
@@ -19,4 +19,23 @@ describe Goldencobra::Article do
     no_url_name_article = Goldencobra::Article.new(@attr.merge(:url_name => ""))
     no_url_name_article.should_not be_valid
   end
+  
+  it "should not display partial in templatefiles" do
+    File.new("#{::Rails.root}/app/views/layouts/tim_test.html.erb", "w")
+    File.new("#{::Rails.root}/app/views/layouts/_partial.html.erb", "w")
+    File.new("#{::Rails.root}/app/views/layouts/_partial_2.html.erb", "w")
+    File.new("#{::Rails.root}/app/views/layouts/12layout.html.erb", "w")
+    
+    Goldencobra::Article.templates_for_select.include?("tim_test").should == true
+    Goldencobra::Article.templates_for_select.include?("_partial").should == false
+    Goldencobra::Article.templates_for_select.include?("_partial_2").should == false
+    Goldencobra::Article.templates_for_select.include?("application").should == true
+    Goldencobra::Article.templates_for_select.include?("12layout").should == true
+    
+    File.delete("#{::Rails.root}/app/views/layouts/tim_test.html.erb")
+    File.delete("#{::Rails.root}/app/views/layouts/_partial.html.erb")
+    File.delete("#{::Rails.root}/app/views/layouts/_partial_2.html.erb")    
+    File.delete("#{::Rails.root}/app/views/layouts/12layout.html.erb")
+  end
+  
 end
