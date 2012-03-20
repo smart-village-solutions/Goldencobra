a9373f0a40e28a686991724527ab3bc979765a21
Marco Metz <marco.metz@gmail.com>
2012-03-20 18:22:02 +0100

    user rights

--- app/admin/roles.rb
+++ app/admin/roles.rb
@@ -1,5 +1,5 @@
 ActiveAdmin.register Goldencobra::Role, :as => "Role" do
-    menu :parent => "Einstellungen", :label => "Rollenverwaltung", :if => proc{can?(:read, Goldencobra::Role)}
+    menu :parent => "Einstellungen", :label => "Rollenverwaltung", :if => proc{can?(:update, Goldencobra::Role)}
     controller.authorize_resource :class => Goldencobra::Role
         
 end
