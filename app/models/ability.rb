fb58abe07c1bc26cc3c1c29b1ae7cc0f0d5eff25
Marco Metz <marco.metz@gmail.com>
2012-03-20 17:57:08 +0100

    ability neu

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -6,12 +6,24 @@ class Ability
     can :read, Goldencobra::Article
     user.roles.each do |role|
       role.permissions.each do |permission|
-        if permission.subject_class == ":all"          
-          can permission.action.to_sym, :all
+        if permission.subject_class == ":all"   
+          if permission.action.include?("not_")
+            cannot permission.action.gsub("not_", "").to_sym, :all
+          else
+            can permission.action.to_sym, :all
+          end
         elsif permission.subject_id.nil?
-          can permission.action.to_sym, permission.subject_class.constantize          
+          if permission.action.include?("not_")
+            cannot permission.action.gsub("not_", "").to_sym, permission.subject_class.constantize          
+          else
+            can permission.action.to_sym, permission.subject_class.constantize          
+          end
         else
-          can permission.action.to_sym, permission.subject_class.constantize, :id => eval(permission.subject_id)
+          if permission.action.include?("not_")
+            cannot permission.action.gsub("not_", "").to_sym, permission.subject_class.constantize, :id => eval(permission.subject_id)
+          else
+            can permission.action.to_sym, permission.subject_class.constantize, :id => eval(permission.subject_id)
+          end
         end
       end
     end
