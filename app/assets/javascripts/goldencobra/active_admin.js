acbb11b5b990aea912d93c0230e31ed08137a570
Holger Frohloff <holger.frohloff@gmail.com>
2012-03-21 18:16:10 +0100

    metadescription hints als tinymce hinzugefügt. muss noch gestyled und angepasst werden 	modified:   app/admin/articles.rb 	modified:   app/assets/javascripts/goldencobra/active_admin.js 	modified:   app/assets/stylesheets/goldencobra/active_admin.css.scss 	modified:   app/models/goldencobra/article.rb   modified:   app/models/goldencobra/metatag.rb

--- app/assets/javascripts/goldencobra/active_admin.js
+++ app/assets/javascripts/goldencobra/active_admin.js
@@ -19,6 +19,17 @@ $(document).ready(function() {
 	});
 	
 	
+	$('.metadescription_hint').tinymce({
+		script_url: "/assets/goldencobra/tiny_mce.js",
+  		mode : "textareas",
+  		theme : "advanced",
+  		theme_advanced_buttons1 : "",
+  		theme_advanced_buttons2 : "",
+  		theme_advanced_buttons3 : "",
+  		theme_advanced_toolbar_location : "bottom",
+  		theme_advanced_toolbar_align : "center",
+  		theme_advanced_resizing : false
+  });
 	
 	//Image Manager
 	$("a#open_goldencobra_image_maganger").bind("click", function(){
