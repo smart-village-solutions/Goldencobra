78fc9719b32d3941c07f9e44ec69de9c199af632
Holger Frohloff <holger.frohloff@gmail.com>
2012-04-02 15:43:52 +0200

    Chosen jQuery added

--- config/initializers/active_admin.rb
+++ config/initializers/active_admin.rb
@@ -115,11 +115,13 @@ ActiveAdmin.setup do |config|
   #
   # To load a stylesheet:
      config.register_stylesheet 'goldencobra/active_admin'
+     config.register_stylesheet 'goldencobra/chosen.css'
      #config.register_stylesheet 'goldencobra/jquery.fileupload-ui.css'
      #config.register_stylesheet 'goldencobra/ikusei_active_admin.css'
       
   # To load a javascript file:
      config.register_javascript 'goldencobra/active_admin.js' 
+     config.register_javascript 'goldencobra/chosen.jquery.min.js' 
      #config.register_javascript 'goldencobra/application.js' 
      config.register_javascript "https://ajax.googleapis.com/ajax/libs/jquery/1.7.1/jquery.min.js"
      config.register_javascript "https://ajax.googleapis.com/ajax/libs/jqueryui/1.8.16/jquery-ui.min.js"
