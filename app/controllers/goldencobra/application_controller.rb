0950b94cace40a30f70a17312104c15f72519bef
Holger Frohloff <holger.frohloff@gmail.com>
2012-04-23 09:52:26 +0200

    cancan rescue 	modified:   app/controllers/goldencobra/application_controller.rb

--- app/controllers/goldencobra/application_controller.rb
+++ app/controllers/goldencobra/application_controller.rb
@@ -1,6 +1,10 @@
 module Goldencobra
   class ApplicationController < ActionController::Base    
-    
+
+    rescue_from CanCan::AccessDenied do |exception|
+      redirect_to root_url, :alert => exception.message
+    end  
+      
     def s(name)
       if name.present?
         Goldencobra::Setting.for_key(name)
