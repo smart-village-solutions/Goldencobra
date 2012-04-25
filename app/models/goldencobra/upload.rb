5012ace8ffdf989fd77afe3832afa6cb788c23c4
Marco Metz <marco.metz@gmail.com>
2012-04-25 14:44:04 +0200

    modified:   Gemfile.lock 	modified:   README.rdoc 	modified:   admin/uploads.rb 	modified:   admin/users.rb 	modified:   app/models/goldencobra/upload.rb 	modified:   goldencobra.gemspec

--- app/models/goldencobra/upload.rb
+++ app/models/goldencobra/upload.rb
@@ -20,6 +20,7 @@ module Goldencobra
                         :styles => { :large => "900x900>",:big => "600x600>", :medium => "300x300>", :thumb => "100x100>", :mini => "50x50>" },
                         :path => ":rails_root/public/system/:attachment/:id/:style/:filename",
                         :url => "/system/:attachment/:id/:style/:filename"
+      before_post_process :image_file?
     end
     
     has_many :article_images, :class_name => Goldencobra::ArticleImage
@@ -33,5 +34,32 @@ module Goldencobra
       result << "(#{self.source}, #{self.rights}) " if self.source.present? || self.rights.present?
       result << "- #{self.created_at}"
     end
+    
+    def unzip_files
+      if self.image_file_name.include?(".zip") && File.exists?(self.image.path)
+        logger.info("===============   is a zip")
+        require 'zip/zip'
+        zipped_files = Zip::ZipFile.open(self.image.path)
+        zipped_files.each do |zipped_file|
+          if zipped_file.file? 
+            zipped_file.extract("tmp/#{self.id}_#{zipped_file.name}")
+            Goldencobra::Upload.create(:image => File.open("tmp/#{self.id}_#{zipped_file.name}"), 
+                        :source => self.source, 
+                        :rights => self.rights, 
+                        :description => self.description, 
+                        :tag_list => self.tag_list.join(", ") )
+            File.delete("tmp/#{self.id}_#{zipped_file.name}")
+          end
+        end 
+        self.destroy
+      end   
+    end
+    
+    def image_file?
+      !(self.image_content_type =~ /^image.*/).nil?
+    end
+    
   end
 end
+
+
