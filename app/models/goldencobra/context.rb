0e602ff8762859c52bc6a4abfebab8463a717799
Marco Metz <marco.metz@gmail.com>
2012-05-04 19:38:57 +0200

    modified:   app/models/goldencobra/context.rb 	modified:   app/views/goldencobra/articles/show.html.erb

--- app/models/goldencobra/context.rb
+++ app/models/goldencobra/context.rb
@@ -1,5 +1,14 @@
 module Goldencobra
   class Context < Radius::Context    
     
+    Parser = []
+    
+    def initialize()
+       super()  
+       Parser.each do |parse_method| 
+         self.send(parse_method)
+       end  
+     end    
+    
   end
 end
