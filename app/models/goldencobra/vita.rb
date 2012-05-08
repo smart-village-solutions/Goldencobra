f19c8939270f5a9727aac47825ebdf258b65a3ae
Marco Metz <marco.metz@gmail.com>
2012-05-08 11:23:01 +0200

    modified:   app/models/goldencobra/article.rb 	modified:   app/models/goldencobra/setting.rb 	modified:   app/models/goldencobra/vita.rb 	modified:   test/unit/goldencobra/vita_test.rb

--- app/models/goldencobra/vita.rb
+++ app/models/goldencobra/vita.rb
@@ -1,3 +1,17 @@
+# == Schema Information
+#
+# Table name: goldencobra_vita
+#
+#  id            :integer(4)      not null, primary key
+#  loggable_id   :integer(4)
+#  loggable_type :string(255)
+#  user_id       :integer(4)
+#  title         :string(255)
+#  description   :text
+#  created_at    :datetime        not null
+#  updated_at    :datetime        not null
+#
+
 module Goldencobra
   class Vita < ActiveRecord::Base
     belongs_to :loggable, :polymorphic => true
--- test/unit/goldencobra/vita_test.rb
+++ test/unit/goldencobra/vita_test.rb
@@ -1,3 +1,17 @@
+# == Schema Information
+#
+# Table name: goldencobra_vita
+#
+#  id            :integer(4)      not null, primary key
+#  loggable_id   :integer(4)
+#  loggable_type :string(255)
+#  user_id       :integer(4)
+#  title         :string(255)
+#  description   :text
+#  created_at    :datetime        not null
+#  updated_at    :datetime        not null
+#
+
 require 'test_helper'
 
 module Goldencobra
