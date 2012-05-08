59c134e376d1690a74687c1c693f0998e951d562
Marco Metz <marco.metz@gmail.com>
2012-05-08 12:11:42 +0200

    modified:   db/migrate/20120327102754_add_default_404_article.rb

--- db/migrate/20120327102754_add_default_404_article.rb
+++ db/migrate/20120327102754_add_default_404_article.rb
@@ -1,11 +1,11 @@
 class AddDefault404Article < ActiveRecord::Migration
   def up
-    pag404 = Goldencobra::Article.new
+    page404 = Goldencobra::Article.new
     page404.content = "Diese Seite existiert leider nicht."
     page404.url_name = "404"
     page404.breadcrumb = "Seite nicht gefunden"
-    pag404.title = "404"
-    pag404.save
+    page404.title = "404"
+    page404.save
   end
 
   def down
