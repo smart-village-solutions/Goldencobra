e319578a0dc56047793e3e29cd05fbdd010f9c95
Marco Metz <marco.metz@gmail.com>
2012-05-16 16:03:55 +0200

    modified:   test/dummy/spec/models/location_spec.rb

--- test/dummy/spec/models/location_spec.rb
+++ test/dummy/spec/models/location_spec.rb
@@ -6,12 +6,14 @@ describe Goldencobra::Location do
     location = Goldencobra::Location.create(:street => "Zossener Str. 55", :city => "Berlin", :zip => "10961")
     location.lat.should_not == nil
     location.lng.should_not == nil
+    Goldencobra::Location.find_by_zip("10961").street.should == "Zossener Str. 55"
   end
 
   it "should have no latitude and longitude because of missing city" do
-    location = Goldencobra::Location.create(:street => "Zossener Str. 55", :city => "", :zip => "10961")
+    location = Goldencobra::Location.create(:street => "Puccinistrasse 40", :city => "", :zip => "13088")
     location.lat.should == nil
     location.lng.should == nil
+    Goldencobra::Location.find_by_zip("13088").street.should == "Puccinistrasse 40"
   end
 
   it "should not have a latitude and longitude" do
