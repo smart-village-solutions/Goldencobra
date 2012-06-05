4ba763072077db431bd9b938a413edee496a71d6
Holger Frohloff <holger.frohloff@gmail.com>
2012-06-05 13:17:05 +0200

    modified:   admin/menues.rb 	modified:   admin/settings.rb 	modified:   admin/uploads.rb 	modified:   admin/users.rb 	modified:   admin/widgets.rb

--- admin/settings.rb
+++ admin/settings.rb
@@ -10,9 +10,7 @@ ActiveAdmin.register Goldencobra::Setting, :as => "Setting"  do
       f.input :value
       f.input :parent_id, :as => :select, :collection => Goldencobra::Setting.all.map{|c| [c.title, c.id]}, :include_blank => true
     end
-    f.inputs "" do
-      f.actions
-    end
+    f.actions
   end
   
   sidebar :overview, only: [:index]  do
