5fe31422284df1ffe4ad212f7237c8121dc1384c
Holger Frohloff <holger.frohloff@gmail.com>
2012-06-15 15:44:41 +0200

    liquid methods fuer articly_type erweitert 	modified:   app/controllers/goldencobra/articles_controller.rb 	modified:   app/models/goldencobra/location.rb

--- app/controllers/goldencobra/articles_controller.rb
+++ app/controllers/goldencobra/articles_controller.rb
@@ -15,8 +15,8 @@ module Goldencobra
     def show
       if @article
         Goldencobra::Article::LiquidParser["current_article"] = @article
-        if @article.article_type.present?
-          Goldencobra::Article::LiquidParser["#{@article.article_type_form_file.downcase}"] = @article.send(@article.article_type_form_file.downcase.to_sym)
+        if @article.article_type.present? && @sym = @article.send(@article.article_type_form_file.downcase.to_sym)
+          Goldencobra::Article::LiquidParser["#{@article.article_type_form_file.downcase}"] = @sym
         end
 
         set_meta_tags :site => Goldencobra::Setting.for_key("goldencobra.page.default_title_tag"),
