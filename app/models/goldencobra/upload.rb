1af301260f0ed70e91163d7929e13a8608477fcf
Holger Frohloff <holger.frohloff@gmail.com>
2012-06-15 17:02:41 +0200

    uploads => attachable
    Uploads are now attachable and polymorphic. So you can use them in many
    different models.
    
    has_many :images, as: :attachable, class_name: Goldencobra::Upload
    	modified:   app/models/goldencobra/upload.rb
    	new file:   db/migrate/20120615150128_add_attachable_to_goldencobra_uploads.rb

--- app/models/goldencobra/upload.rb
+++ app/models/goldencobra/upload.rb
@@ -27,7 +27,7 @@ module Goldencobra
     has_many :article_images, :class_name => Goldencobra::ArticleImage
     has_many :articles, :through => :article_images
     has_many :imports, :class_name => Goldencobra::Import
-    
+    belongs_to :attachable, polymorphic: true
     
     def title
       "#{self.image_file_name} (#{image_content_type})"
