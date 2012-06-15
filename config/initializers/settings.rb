ba6987dbd9737e1c106297a103c755f9b3558c29
Marco Metz <marco.metz@gmail.com>
2012-06-15 15:59:10 +0200

    rails generate working now always

--- config/initializers/settings.rb
+++ config/initializers/settings.rb
@@ -1,3 +1,4 @@
+require 'goldencobra/acts_as_setting'
 Rails.application.config.to_prepare do
   if ActiveRecord::Base.connection.table_exists?("goldencobra_settings")
     Goldencobra::Setting.import_default_settings(Goldencobra::Engine.root + "config/settings.yml")
