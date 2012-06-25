6b5ca4162431961f6ada9e8c85a2eb7a5d789b0f
Holger Frohloff <holger.frohloff@gmail.com>
2012-06-25 13:07:33 +0200

    sort order extension for article types 	modified:   app/controllers/goldencobra/articles_controller.rb

--- app/controllers/goldencobra/articles_controller.rb
+++ app/controllers/goldencobra/articles_controller.rb
@@ -27,9 +27,12 @@ module Goldencobra
               @list_of_articles = @list_of_articles.flatten.shuffle!
             elsif @article.sort_order == "Alphabetical"
               @list_of_articles = @list_of_articles.flatten.sort_by!{|article| article.title }
-            elsif @article.send(@article.sort_order)
+            elsif @article.respond_to?(@article.sort_order)
               sort_order = @article.sort_order.downcase
-              @list_of_articles = @list_of_articles.flatten.sort_by!{|article| article.send(sort_order) ? article.send(sort_order) : article }
+              @list_of_articles = @list_of_articles.flatten.sort_by!{|article| article.respond_to?(sort_order) ? article.send(sort_order) : article }
+            elsif @article.sort_order.include?(".")
+              sort_order = @article.sort_order.downcase.split(".")
+              @list_of_articles = @list_of_articles.flatten.sort_by!{|article| sort_order.inject(article) {|result, message| result.send message} }
             end
             if @article.reverse_sort
               @list_of_articles = @list_of_articles.reverse
