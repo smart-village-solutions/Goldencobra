0642ec0d7552c02f763582a95ccf497c927623c4
Marco Metz <marco.metz@gmail.com>
2012-07-02 17:30:33 +0200

    modified:   app/models/goldencobra/article.rb 	deleted:    db/migrate/20120702150955_add_extendable_to_goldencobra_articles.rb

--- db/migrate/20120702150955_add_extendable_to_goldencobra_articles.rb
+++
@@ -1,6 +0,0 @@
-class AddExtendableToGoldencobraArticles < ActiveRecord::Migration
-  def change
-    add_column :goldencobra_articles, :expandable_id, :integer
-    add_column :goldencobra_articles, :expandable_type, :string
-  end
-end
