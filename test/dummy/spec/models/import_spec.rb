7b20514de3891c01c899b7f79c18d9f7d9ffa0f1
Holger Frohloff <holger.frohloff@gmail.com>
2012-07-04 11:26:01 +0200

    whtitespace 	modified:   admin/articles.rb 	modified:   test/dummy/spec/models/import_spec.rb

--- test/dummy/spec/models/import_spec.rb
+++ test/dummy/spec/models/import_spec.rb
@@ -5,11 +5,11 @@ describe Goldencobra::Import do
 
   describe "Upload CSV file" do
     it "should be successful" do
-      CSV.open("tmp/test_import.csv", "wb") do |csv|
+      CSV.open("test_import.csv", "wb") do |csv|
         csv << ["Titel der Seite", "haupttext", "teaser text"]
         csv << ["erster titel", "ein text", "ein teaser"]
       end
-      csv_file = File.open("tmp/test_import.csv", "r")
+      csv_file = File.open("test_import.csv", "r")
       new_upload = Goldencobra::Upload.create(:image => csv_file, :description => "CSV Datei")
       importer = Goldencobra::Import.new(:target_model => "Goldencobra::Article", :separator => ",", :upload_id => new_upload.id)
       importer.assignment = {"title" => "0", "content" => "1", "teaser" => "2"}
@@ -25,13 +25,13 @@ describe Goldencobra::Import do
 
   describe "Upload CSV file with wron entries" do
     it "should be successful" do
-      CSV.open("tmp/test_import.csv", "wb") do |csv|
+      CSV.open("test_import.csv", "wb") do |csv|
         csv << ["Titel der Seite", "haupttext", "teaser text"]
         csv << ["erster titel", "ein text", "ein teaser"]
         csv << ["", "ein leerer text", "ein doofer teaser"]
         csv << ["noch erster titel", "ein weiterer text", "ein anderer teaser"]
       end
-      csv_file = File.open("tmp/test_import.csv", "r")
+      csv_file = File.open("test_import.csv", "r")
       new_upload = Goldencobra::Upload.create(:image => csv_file, :description => "CSV Datei")
       importer = Goldencobra::Import.new(:target_model => "Goldencobra::Article", :separator => ",", :upload_id => new_upload.id)
       importer.assignment = {"title" => "0", "content" => "1", "teaser" => "2"}
