005fab014ba87a4d5860ea2d7b44fdba24bea88f
Holger Frohloff <holger.frohloff@gmail.com>
2012-07-11 17:03:30 +0200

    updated active_admin 	modified:   Gemfile.lock 	modified:   test/dummy/config/environments/production.rb

--- test/dummy/config/environments/production.rb
+++ test/dummy/config/environments/production.rb
@@ -9,7 +9,7 @@ Dummy::Application.configure do
   config.action_controller.perform_caching = true
 
   # Disable Rails's static asset server (Apache or nginx will already do this)
-  config.serve_static_assets = true
+  config.serve_static_assets = false
 
   # Compress JavaScripts and CSS
   config.assets.compress = true
@@ -20,7 +20,6 @@ Dummy::Application.configure do
   # Generate digests for assets URLs
   config.assets.digest = true
 
-  config.static_cache_control = "public, max-age=3600"
 
   # Defaults to Rails.root.join("public/assets")
   # config.assets.manifest = YOUR_PATH
