af21154647ad459ffda3d27d505a36503325c383
Marco Metz <marco.metz@gmail.com>
2012-07-24 11:11:29 +0200

    modified:   app/helpers/goldencobra/articles_helper.rb 	modified:   app/models/goldencobra/menue.rb

--- app/helpers/goldencobra/articles_helper.rb
+++ app/helpers/goldencobra/articles_helper.rb
@@ -127,7 +127,7 @@ module Goldencobra
             content_level << navigation_menu_helper(subchild, depth, current_depth, options)
         end
         if content_level.present?
-          child_link = child_link + content_tag(:ul, raw(content_level), :class => "level_#{current_depth} children_#{child.children.active.count}" )
+          child_link = child_link + content_tag(:ul, raw(content_level), :class => "level_#{current_depth} children_#{child.children.active.visible.count}" )
         end
       end  
       return content_tag(:li, raw(child_link), :class => "#{child.has_active_child?(request) ? 'has_active_child' : ''} #{child.is_active?(request) ? 'active' : ''} #{child.css_class.gsub(/\W/,' ')}".squeeze(' ').strip)
