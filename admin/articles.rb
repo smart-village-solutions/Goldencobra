71d70a6b29382c8f50508de10f09300264e2ab4e
ikusei Praktikant <praktikant@ikusei.de>
2012-08-08 17:30:25 +0200

    sortierung bearbeiten 	modified:   ../../admin/articles.rb

--- admin/articles.rb
+++ admin/articles.rb
@@ -94,7 +94,7 @@ ActiveAdmin.register Goldencobra::Article, :as => "Article" do
 
   index do
     selectable_column
-    column I18n.t("name", :scope => [:goldencobra, :menue]), :sortable => :breadcrumb do |article|
+    column I18n.t("name", :scope => [:goldencobra, :menue]), sortable: :breadcrumb do |article|
       content_tag("span", link_to(truncate(article.breadcrumb_name, :length => 40), edit_admin_article_path(article), :class => "member_link edit_link"), :class => article.startpage ? "startpage" : "")
     end
     column :url, sortable: :public_url  do |article|
