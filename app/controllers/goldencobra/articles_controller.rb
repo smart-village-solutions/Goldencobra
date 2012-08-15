8d4e62c2c84a5a85798df1df91b935081630de78
Marco Metz <marco.metz@gmail.com>
2012-08-15 19:03:55 +0200

    method missing

--- app/controllers/goldencobra/articles_controller.rb
+++ app/controllers/goldencobra/articles_controller.rb
@@ -34,7 +34,7 @@ module Goldencobra
               @list_of_articles = @list_of_articles.flatten.sort_by!{|article| article.respond_to?(sort_order) ? article.send(sort_order) : article }
             elsif @article.sort_order.include?(".")
               sort_order = @article.sort_order.downcase.split(".")
-              @list_of_articles = @list_of_articles.flatten.sort_by!{|article| sort_order.inject(article){|result, message| result.send message if result.respond_to?(message)} }
+              @list_of_articles = @list_of_articles.flatten.sort_by!{|article| sort_order.inject(article){|result, message| result.send(message) if result.present? && a=result.send(message) && a.present?  } }
             end
             if @article.reverse_sort
               @list_of_articles = @list_of_articles.reverse
