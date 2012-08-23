c16b202bfe6aceb2addb3faeb3479a419d07009a
ikusei Praktikant <praktikant@ikusei.de>
2012-08-23 10:56:51 +0200

    modified:   admin/widgets.rb

--- admin/widgets.rb
+++ admin/widgets.rb
@@ -6,7 +6,7 @@ ActiveAdmin.register Goldencobra::Widget, as: "Widget" do
   scope "offline", :inactive
   scope "defaults", :default
   Goldencobra::Widget.tag_counts_on(:tags).map(&:name).each do |wtag|
-    if Goldencobra::Widget.tag_counts_on(:tags).count > 1
+    if (Goldencobra::Widget.tag_counts_on(:tags).count > 1)
       scope(I18n.t(wtag, :scope => [:goldencobra, :widget_types], :default => wtag)){ |t| t.tagged_with(wtag) }
     end
   end
