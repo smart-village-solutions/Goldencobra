0a08a56ff1314626dc6186fb4d5c138e1567eb80
Marco Metz <marco.metz@gmail.com>
2012-08-23 15:03:39 +0200

    modified:   app/controllers/goldencobra/articles_controller.rb 	modified:   config/initializers/geocoder.rb

--- config/initializers/geocoder.rb
+++ config/initializers/geocoder.rb
@@ -1,5 +1,7 @@
 Geocoder::Configuration.lookup = :google
+#Geocoder::Configuration.api_key = "AIzaSyCFREOloLh2CTRpELzzGJJSEPV-LKyAfjY"
 Geocoder::Configuration.api_key = ""
 Geocoder::Configuration.timeout = 5
-Geocoder::Configuration.use_https = true
+Geocoder::Configuration.use_https = false
 Geocoder::Configuration.language = :de
+Geocoder::Configuration.cache = Redis.new
