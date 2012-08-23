141c42d4db3bee2c9716efcfeb312a7607a6de1b
Marco Metz <marco.metz@gmail.com>
2012-08-23 09:27:23 +0200

    modified:   app/assets/javascripts/goldencobra/active_admin.js

--- app/assets/javascripts/goldencobra/active_admin.js
+++ app/assets/javascripts/goldencobra/active_admin.js
@@ -1,7 +1,7 @@
 //= require active_admin/base
 //= require goldencobra/keymaster
 
-$(document).ready(function() {	
+$(document).ready(function() {
 	$('textarea.tinymce').tinymce({
 		script_url: "/assets/goldencobra/tiny_mce.js",
   		mode : "textareas",
@@ -11,8 +11,9 @@ $(document).ready(function() {
   		theme_advanced_buttons3 : "",
   		theme_advanced_toolbar_location : "top",
   		theme_advanced_toolbar_align : "center",
-  		theme_advanced_resizing : false, 
+  		theme_advanced_resizing : false,
 		relative_urls : true,
+    convert_urls : false,
 		theme_advanced_blockformats : "p,h1,h2,h3,div",
 		plugins : "fullscreen,autolink,paste",
 		dialog_type : "modal",
@@ -26,9 +27,9 @@ $(document).ready(function() {
   });
   $("div.overview-sidebar div.folder").trigger("click");
 
-  //Add Button Background Jobs zu Settings 
+  //Add Button Background Jobs zu Settings
 	$('#einstellungen ul').append("<li><a href='/admin/background'>Background Jobs</a></li>")
-	
+
 	$('.metadescription_hint').tinymce({
 		script_url: "/assets/goldencobra/tiny_mce.js",
   		mode : "textareas",
@@ -50,23 +51,23 @@ $(document).ready(function() {
     var editor = tinyMCE.getInstanceById('metadescription-tinymce');
     editor.getBody().style.backgroundColor = "#F4f4f4";
   }
-	
+
 	//Image Manager
 	$("a#open_goldencobra_image_maganger").bind("click", function(){
 		$("#goldencobra_image_maganger").fadeToggle();
 		return false;
 	});
-	
+
 	$("#goldencobra_image_maganger").draggable({
 		handle: ".header"
 	});
-	
+
 	$("#goldencobra_image_maganger div.header div.close").bind("click", function(){
 		$("#goldencobra_image_maganger").fadeOut();
 	});
-	
+
 	$('#footer').html("<p>Goldencobra</p>")
-	
+
 	//die fieldsets bekommen einen button zum auf und zu klappen
 	$('div#main_content fieldset.foldable legend').prepend("<div class='foldable_icon_wrapper'><div class='foldable_icon'></div></div>")
 	$('div#main_content fieldset.foldable legend').bind("click", function(){
@@ -74,8 +75,8 @@ $(document).ready(function() {
 		$(this).closest("fieldset").find('ol').slideToggle();
 	});
 	$('div#main_content fieldset.foldable.closed legend').trigger("click");
-	
-	
+
+
 	//die sidebar_section bekommen einen button zum auf und zu klappen
 	$('div#sidebar div.sidebar_section h3').prepend("<div class='foldable_icon_wrapper'><div class='foldable_icon'></div></div>")
 	$('div#sidebar div.sidebar_section h3').bind("click", function(){
@@ -83,14 +84,14 @@ $(document).ready(function() {
 		$(this).closest(".sidebar_section").find('.panel_contents').slideToggle();
 	});
 	$('div#sidebar div.sidebar_section:not(#overview_sidebar_section) h3').trigger("click");
-	
+
 	$(".chzn-select").chosen();
   $(".chzn-select-deselect").chosen({allow_single_deselect:true});
 
   //Menuepunkte bekommen eine funktion zum auf und zu klappen
   $('div#overview_sidebar div.title a').bind("click", function(){
   	$(this).children("ul").hide();
-  	
+
   });
   	$('div#overview_sidebar div.title a').trigger("click");
 
@@ -115,8 +116,8 @@ $(document).ready(function() {
   });
 
 
-	
- 
+
+
 
 
 });
