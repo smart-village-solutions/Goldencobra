904cd090be6c85c331008050363c062badd7cc01
ikusei Praktikant <praktikant@ikusei.de>
2012-09-11 16:33:34 +0200

    modified:   admin/uploads.rb

--- admin/uploads.rb
+++ admin/uploads.rb
@@ -46,7 +46,13 @@ ActiveAdmin.register Goldencobra::Upload, :as => "Upload"  do
 	      link_to(raw("entpacken"), unzip_file_admin_upload_path(upload))
 	    end
 	  end
-    default_actions
+    column "" do |upload|
+      result = ""
+      result += link_to(t(:view), upload.image, :class => "member_link edit_link view", :title => "Vorschau")
+      result += link_to(t(:edit), edit_admin_upload_path(upload), :class => "member_link edit_link edit", :title => "bearbeiten")
+      result += link_to(t(:delete), admin_upload_path(upload), :method => :DELETE, :confirm => t("delete_article", :scope => [:goldencobra, :flash_notice]), :class => "member_link delete_link delete", :title => "loeschen")
+      raw(result)
+    end
   end
 
   show do
