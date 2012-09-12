0383fd2bac8c12829e01fd0e2219e0471c42d885
Holger Frohloff <holger.frohloff@gmail.com>
2012-09-12 15:50:12 +0200

    list of articles in json output 	modified:   app/controllers/goldencobra/articles_controller.rb

--- app/controllers/goldencobra/articles_controller.rb
+++ app/controllers/goldencobra/articles_controller.rb
@@ -82,7 +82,10 @@ module Goldencobra
           respond_to do |format|
             format.html { render layout: layout_to_render }
             format.rss
-            format.json { render json: {article: @article, list_of_articles: @list_of_articles} }
+            format.json do
+              @article["list_of_articles"] = @list_of_articles
+              render json: @article.to_json
+            end
           end
         end
       elsif @article && @article.external_url_redirect.present?
