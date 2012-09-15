845862c4f9d00b6d349f57f89e82c46eefda606b
Marco Metz <marco.metz@gmail.com>
2012-09-15 17:54:38 +0200

    modified:   app/models/goldencobra/role_user.rb

--- app/models/goldencobra/role_user.rb
+++ app/models/goldencobra/role_user.rb
@@ -1,5 +1,6 @@
 module Goldencobra
   class RoleUser < ActiveRecord::Base
-  
+    belongs_to :user, :class_name => User, :foreign_key => :user_id
+    belongs_to :role, :class_name => Goldencobra::Role
   end
 end
