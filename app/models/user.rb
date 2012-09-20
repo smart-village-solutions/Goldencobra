8592e52150838e2bd9c6b9b38513407ca11bc43b
Holger Frohloff <holger.frohloff@gmail.com>
2012-09-20 16:40:19 +0200

    nested attributes for user.vita_steps 	modified:   app/models/user.rb

--- app/models/user.rb
+++ app/models/user.rb
@@ -51,6 +51,7 @@ class User < ActiveRecord::Base
   attr_accessible :email, :password, :password_confirmation, :remember_me, :gender, :title, :firstname, :lastname, :function, :phone, :fax, :facebook, :twitter, :linkedin, :xing, :googleplus, :role_ids
   has_and_belongs_to_many :roles, :join_table => "goldencobra_roles_users", :class_name => Goldencobra::Role, :include => [:permissions]
   has_many :vita_steps, :as => :loggable, :class_name => Goldencobra::Vita
+  accepts_nested_attributes_for :vita_steps, allow_destroy: true, reject_if: lambda { |a| a[:description].blank? }
 
   before_save :ensure_authentication_token
 
