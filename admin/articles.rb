7f13b3bdab8d1c43e9a1ce94237fcf1036f2aac0
Marco Metz <marco.metz@gmail.com>
2012-09-26 16:04:07 +0200

    modified:   admin/articles.rb

--- admin/articles.rb
+++ admin/articles.rb
@@ -98,7 +98,7 @@ ActiveAdmin.register Goldencobra::Article, :as => "Article" do
       f.inputs "Medien", :class => "foldable closed inputs"  do
         f.has_many :article_images do |ai|
           ai.input :image, :as => :select, :collection => Goldencobra::Upload.all.map{|c| [c.complete_list_name, c.id]}, :input_html => { :class => 'article_image_file chzn-select'}, :label => "Bild ausw&auml;hlen"
-          ai.input :position, :as => :select, :collection => Goldencobra::Setting.for_key("goldencobra.article.image_positions").split(",")
+          ai.input :position, :as => :select, :collection => Goldencobra::Setting.for_key("goldencobra.article.image_positions").split(","), :include_blank => false
           ai.input :_destroy, :as => :boolean
         end
       end
