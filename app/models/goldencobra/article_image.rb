b81632493b01cc8a2bd10458207e1385514e65eb
Marco Metz <marco.metz@gmail.com>
2012-09-26 15:58:23 +0200

    modified:   app/models/goldencobra/article_image.rb

--- app/models/goldencobra/article_image.rb
+++ app/models/goldencobra/article_image.rb
@@ -7,11 +7,13 @@
 #  image_id   :integer(4)
 #  created_at :datetime        not null
 #  updated_at :datetime        not null
+#  position :string
 #
 
 module Goldencobra
   class ArticleImage < ActiveRecord::Base
     belongs_to :article
     belongs_to :image, :class_name => Goldencobra::Upload, :foreign_key => "image_id"
+
   end
 end
