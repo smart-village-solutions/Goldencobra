5dfda9e7032d024b9943a577827067be1dc35926
Marco Metz <marco.metz@gmail.com>
2012-09-27 13:46:38 +0200

    no access redirect to login

--- config/initializers/active_admin_devise.rb
+++ config/initializers/active_admin_devise.rb
@@ -1,22 +1,22 @@
 require "activeadmin"
-ApplicationController.class_eval do  
+ApplicationController.class_eval do
     layout :layout_by_resource_for_user_model
 
-    
+
     def current_ability
         @current_ability ||= Ability.new(current_user)
     end
-    
+
     rescue_from CanCan::AccessDenied do |exception|
-        redirect_to admin_dashboard_path, :alert => exception.message
+        redirect_to new_user_session_path, :alert => exception.message
     end
 
     protected
-    def layout_by_resource_for_user_model 
+    def layout_by_resource_for_user_model
       if devise_controller? && resource_name == :user
-        "goldencobra/active_admin_resque" # we emulate the active_admin layout for consistancy 
+        "goldencobra/active_admin_resque" # we emulate the active_admin layout for consistancy
       else
         "application"
-      end 
+      end
     end
 end
