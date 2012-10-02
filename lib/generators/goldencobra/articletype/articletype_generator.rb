9361c4cbf70e6473b8a72e6430a0acac666eaa86
Marco Metz <marco.metz@gmail.com>
2012-10-02 10:52:40 +0200

    Articeltype Generator created

---
+++ lib/generators/goldencobra/articletype/articletype_generator.rb
@@ -0,0 +1,22 @@
+module Goldencobra
+  module Generators
+    class ArticletypeGenerator < Rails::Generators::NamedBase
+      source_root File.expand_path('../templates', __FILE__)
+      argument :model_attributes, type: :array, default: [], banner: "model:attributes"
+
+      def create_articletype_files
+        puts "Generating Model #{name} #{model_attributes}"
+        generate("model", "#{name} #{model_attributes.join(' ')}")
+        rake("db:migrate")
+      end
+
+      def create_partials
+        copy_file 'index.html.erb', "app/views/articletypes/#{name.underscore}/_index.html.erb"
+        copy_file 'show.html.erb', "app/views/articletypes/#{name.underscore}/_show.html.erb"
+        template 'edit_show.html.erb', "app/views/articletypes/#{name.underscore}/_edit_show.html.erb"
+        copy_file 'edit_index.html.erb', "app/views/articletypes/#{name.underscore}/_edit_index.html.erb"
+      end
+
+    end
+  end
+end
