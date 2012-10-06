cb4466c667890f3506b95defdb18d5c5c143ae9b
Marco Metz <marco.metz@gmail.com>
2012-10-06 12:02:49 +0200

    commentable to articles added

--- admin/articles.rb
+++ admin/articles.rb
@@ -86,6 +86,7 @@ ActiveAdmin.register Goldencobra::Article, :as => "Article" do
         f.input :enable_social_sharing, :label => t("Display Social Sharing actions"), :hint => "Sollen Besucher die actions angezeigt bekommen, um diesen Artikel in den Sozialen Netzwerken zu verbreiten?"
         f.input :robots_no_index, :hint => "Um bei Google nicht in Konkurrenz zu anderen wichtigen Einzelseiten der eigenen Webseite zu treten, kann hier Google mitgeteilt werden, diese Seite nicht zu indizieren"
         f.input :cacheable, :as => :boolean, :hint => "Dieser Artikel darf im Cache liegen"
+        f.input :commentable, :as => :boolean, :hint => "Kommentarfunktion für diesen Artikel aktivieren?"
         f.input :dynamic_redirection, :as => :select, :collection => Goldencobra::Article::DynamicRedirectOptions.map{|a| [a[1], a[0]]}, :include_blank => false
         f.input :external_url_redirect
         f.input :redirection_target_in_new_window
