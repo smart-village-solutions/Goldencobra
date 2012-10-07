6df118cfdd5d43cbe5efbea548d6f0c4a93de50b
Marco Metz <marco.metz@gmail.com>
2012-10-07 14:54:09 +0200

    default date for active_since

--- db/migrate/20121007125002_add_activesince_to_goldencobra_articles.rb
+++ db/migrate/20121007125002_add_activesince_to_goldencobra_articles.rb
@@ -1,5 +1,5 @@
 class AddActivesinceToGoldencobraArticles < ActiveRecord::Migration
   def change
-    add_column :goldencobra_articles, :active_since, :datetime
+    add_column :goldencobra_articles, :active_since, :datetime, :default => (Time.now - 1.week)
   end
 end
