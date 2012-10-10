9b11d1589fdbc3aacbef941ea6b6cd038090ca7b
Holger Frohloff <holger.frohloff@gmail.com>
2012-10-10 12:50:59 +0200

    Farbraum auf sRGB geändert 	modified:   app/models/goldencobra/upload.rb

--- app/models/goldencobra/upload.rb
+++ app/models/goldencobra/upload.rb
@@ -23,7 +23,7 @@ module Goldencobra
                         :styles => { :large => ["900x900>", :jpg], :big => ["600x600>",:jpg], :medium => ["300x300>",:jpg], :thumb => ["100x100>", :jpg], :mini => ["50x50>",:jpg] },
                         :path => ":rails_root/public/system/:attachment/:id/:style/:filename",
                         :url => "/system/:attachment/:id/:style/:filename",
-                        :convert_options => { :all => '-auto-orient -strip -colorspace RGB -flatten' }
+                        :convert_options => { :all => '-auto-orient -strip -colorspace sRGB -flatten' }
       before_post_process :image_file?
     end
 
