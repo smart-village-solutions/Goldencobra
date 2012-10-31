17356f4ee3687565ec6fb4aef0447f796075033c
Marco Metz <marco.metz@gmail.com>
2012-10-31 16:32:04 +0100

    bugtracker

--- app/helpers/goldencobra/application_helper.rb
+++ app/helpers/goldencobra/application_helper.rb
@@ -1,12 +1,21 @@
 module Goldencobra
-  module ApplicationHelper    
+  module ApplicationHelper
     include Goldencobra::ArticlesHelper
-    
+
     def s(name)
       if name.present?
         Goldencobra::Setting.for_key(name)
       end
     end
-    
+
+    def bugtracker
+      user_mod = Goldencobra::Setting.for_key("goldencobra.bugherd.user")
+      role_mod = Goldencobra::Setting.for_key("goldencobra.bugherd.role")
+      bugherd_api = Goldencobra::Setting.for_key("goldencobra.bugherd.api")
+      if bugherd_api.present? && user_mod.present? && role_mod.present? && eval("#{user_mod} && #{user_mod}.has_role?('#{role_mod}')")
+        render :partial => "goldencobra/articles/bugherd", :locals => {:bugherd_api => bugherd_api}
+      end
+    end
+
   end
 end
