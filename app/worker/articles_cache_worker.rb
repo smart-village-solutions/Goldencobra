7672e963900afbb094109576cb056e19f6430e7c
Marco Metz <marco.metz@gmail.com>
2012-11-12 14:11:53 +0100

    modified:   app/worker/articles_cache_worker.rb 	modified:   config/settings.yml

--- app/worker/articles_cache_worker.rb
+++ app/worker/articles_cache_worker.rb
@@ -3,9 +3,17 @@ class ArticlesCacheWorker
   sidekiq_options queue: "default"
 
   def perform
+    #cache leeren
     Goldencobra::Article.active.each do |article|
       article.updated_at = Time.now
       article.save
+    end
+    #Alte Versionen von has_paper_trail löschen (https://github.com/airblade/paper_trail)
+    if ActiveRecord::Base.connection.table_exists?("goldencobra_settings")
+      if Goldencobra::Setting.for_key("goldencobra.remove_old_versions.active") == "true"
+        weekcount = Goldencobra::Setting.for_key("goldencobra.remove_old_versions.weeks").to_i
+        Version.delete_all ["created_at < ?", weekcount.weeks.ago]
+      end
     end
   end
 
