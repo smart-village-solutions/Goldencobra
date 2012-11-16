9b7b11fc7da8acf5d4d05b174c454a47a302285c
Marco Metz <marco.metz@gmail.com>
2012-11-16 15:23:49 +0100

    modified:   app/controllers/goldencobra/articles_controller.rb

--- app/controllers/goldencobra/articles_controller.rb
+++ app/controllers/goldencobra/articles_controller.rb
@@ -275,8 +275,14 @@ module Goldencobra
         if Goldencobra::Setting.for_key("goldencobra.geocode_ip_address") == "true"
           if session[:user_location].blank?
             begin
-              @ip_result = Geokit::Geocoders::MultiGeocoder.geocode(request.remote_ip)
-              session[:user_location] = @ip_result
+              if request.present? && request.remote_ip.present? && request.remote_ip =~ /\b\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}\b/
+                @ip_result = Geokit::Geocoders::MultiGeocoder.geocode(request.remote_ip)
+                session[:user_location] = @ip_result
+              else
+                logger.error("***********")
+                logger.error(request.remote_ip)
+                @ip_result = nil
+              end
             rescue
               @ip_result = nil
             end
