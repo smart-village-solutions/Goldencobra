baffc0612f7cee45e6878b68b09d6d323d7c0700
Marco Metz <marco.metz@gmail.com>
2012-11-22 15:35:46 +0100

    modified:   admin/goldencobra_infos.rb 	modified:   app/assets/javascripts/goldencobra/active_admin.js

--- app/assets/javascripts/goldencobra/active_admin.js
+++ app/assets/javascripts/goldencobra/active_admin.js
@@ -102,19 +102,19 @@ $(document).ready(function() {
 	return false;
   });
 
-  $("#title_bar .action_items a[href$='edit']").append(" (⌘-E)")
+  $("#title_bar .action_items a[href$='edit']").append(" (⌘-E)");
   key('⌘+e, ctrl+e', function(){
-	target = $("#title_bar .action_items a[href$='edit']").attr("href");
-	window.location = target;
-	return false;
+	 target = $("#title_bar .action_items a[href$='edit']").attr("href");
+	 window.location = target;
+	 return false;
   });
 
-  $("#title_bar .action_items a[href$='revert']").append(" (⌘-Z)")
-  key('⌘+z, ctrl+z', function(){
-	target = $("#title_bar .action_items a[href$='revert']").attr("href");
-	window.location = target;
-	return false;
-  });
+ //  $("#title_bar .action_items a[href$='revert']").append(" (⌘-Z)");
+ //  key('⌘+z, ctrl+z', function(){
+	// target = $("#title_bar .action_items a[href$='revert']").attr("href");
+	// window.location = target;
+	// return false;
+ //  });
 
 
 
