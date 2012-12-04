bb1144e337b24f8d708cca85eb9efc6a79b72d65
Marco Metz <marco.metz@gmail.com>
2012-12-04 14:48:41 +0100

    article hooks

---
+++ test/dummy/app/controllers/extend_goldencobra_articles_controller.rb
@@ -0,0 +1,19 @@
+module ExtendGoldencobraArticlesController
+
+  def before_init
+    ::Rails.logger.info("********************************** - BEFORE-INIT")
+  end
+
+  def after_init
+    ::Rails.logger.info("********************************** - AFTER-INIT")
+  end
+
+  def after_index
+    ::Rails.logger.info("********************************** - AFTER-INDEX")
+  end
+
+  def before_render
+    ::Rails.logger.info("********************************** - BEFORE-RENDER")
+  end
+
+end
