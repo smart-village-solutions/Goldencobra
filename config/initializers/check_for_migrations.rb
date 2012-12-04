de62d00cb5b06f06bfaa997ca792d83e10fe9cdd
Marco Metz <marco.metz@gmail.com>
2012-12-04 16:18:00 +0100

    modified:   config/initializers/check_for_migrations.rb

--- config/initializers/check_for_migrations.rb
+++ config/initializers/check_for_migrations.rb
@@ -1,9 +1,19 @@
 unless ::Rails.root.to_s.include?("/test/dummy")
-  all_files_exists = true
+  all_files_exists = []
   Dir.foreach(File.join(Goldencobra::Engine.root,"db", "migrate")) do |f|
-     all_files_exists = false if !File.exists?(File.join(::Rails.root,"db","migrate",f.split("_")[1..-1].join("_")))
+    file_to_search = f.split(".")[0].split("_")[1..-1].join("_")
+    single_all_files_exists = []
+    Dir.foreach(File.join(::Rails.root,"db", "migrate")) do |g|
+      compare_with = g.split(".")[0].split("_")[1..-1].join("_")
+      if compare_with == file_to_search
+        single_all_files_exists << true
+      else
+        single_all_files_exists << false
+      end
+    end
+    all_files_exists << single_all_files_exists.include?(true)
   end
-  if all_files_exists == false
+  if all_files_exists.include?(false)
     raise "There are new migrations to install: rake goldencobra:install:migrations"
   end
 end
