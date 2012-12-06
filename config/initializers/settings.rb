6c3a14f4a5976daf666b701e04a5718aa01d59ec
Marco Metz <marco.metz@gmail.com>
2012-12-06 11:41:44 +0100

    Goldencobra settings initilaizer schaut nun auch automatisch ob eine settings.yml im rails root/conf verzeichnis liegt. Ein zusätzlicher initilaizer ist nun nicht mehr notwendig 	modified:   ../../config/initializers/settings.rb

--- config/initializers/settings.rb
+++ config/initializers/settings.rb
@@ -2,5 +2,8 @@ require 'goldencobra/acts_as_setting'
 Rails.application.config.to_prepare do
   if ActiveRecord::Base.connection.table_exists?("goldencobra_settings")
     Goldencobra::Setting.import_default_settings(Goldencobra::Engine.root + "config/settings.yml")
+    if File.exists?(::Rails.root + "config/settings.yml")
+      Goldencobra::Setting.import_default_settings(::Rails.root + "config/settings.yml")
+    end
   end
 end
