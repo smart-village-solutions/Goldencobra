f96d8baaa1a33d8139956b1610204c272f886525
Holger Frohloff <holger.frohloff@gmail.com>
2012-12-06 16:26:55 +0100

    Api with Tokens controller v1

---
+++ test/dummy/spec/controllers/api/v1/tokens_controller_spec.rb
@@ -0,0 +1,35 @@
+require 'spec_helper'
+
+describe Goldencobra::Api::V1::TokensController do
+  before(:each) { @routes = Goldencobra::Engine.routes }
+  describe '#create' do
+    it 'should require an email and password' do
+      post :create, format: :json
+      response.response_code.should == 400
+    end
+
+    before do
+      @visitor = Visitor.create(email: 'john.doe@foo.bar',
+                                password: '123456',
+                                password_confirmation: '123456',
+                                agb: true)
+    end
+
+    it "should return a user token given correct credentials" do
+      token_to_json = { token: @visitor.authentication_token }.to_json
+
+      post :create, format: :json, email: 'john.doe@foo.bar', password: '123456'
+      response.body.should == token_to_json
+    end
+
+    it 'should render a 401 if password is wrong' do
+      post :create, format: :json, email: 'john.doe@foo.bar', password: '12345'
+      response.response_code.should == 401
+    end
+
+    it 'should render a 401 if email is wrong' do
+      post :create, format: :json, email: 'john@foo.bar', password: '123456'
+      response.response_code.should == 401
+    end
+  end
+end
