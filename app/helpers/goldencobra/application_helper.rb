f5b2b3a35984d14e00fed4e53dea15d6537082da
Holger Frohloff <holger.frohloff@gmail.com>
2012-12-11 13:50:47 +0100

    Edit article link in frontend if current_user is admin

--- app/helpers/goldencobra/application_helper.rb
+++ app/helpers/goldencobra/application_helper.rb
@@ -17,5 +17,12 @@ module Goldencobra
       end
     end
 
+    def edit_article_link
+      user_mod = Goldencobra::Setting.for_key("goldencobra.article.edit_link.user")
+      role_mod = Goldencobra::Setting.for_key("goldencobra.article.edit_link.role")
+      if user_mod.present? && role_mod.present? && eval("#{user_mod} && #{user_mod}.has_role?('#{role_mod}')")
+        render :partial => "goldencobra/articles/edit_article_link"
+      end
+    end
   end
 end
