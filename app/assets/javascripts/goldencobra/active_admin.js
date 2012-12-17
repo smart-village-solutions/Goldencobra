0fde269a4582fe3021f7ba715f7f484885ede82d
Marco Metz <marco.metz@gmail.com>
2012-12-17 17:44:02 +0100

    ?? sollte sein &&

--- app/assets/javascripts/goldencobra/active_admin.js
+++ app/assets/javascripts/goldencobra/active_admin.js
@@ -123,3 +123,40 @@ $(document).ready(function() {
 
 });
 
+
+//Notifications
+function notify(title,body,token) {
+    // check for notification compatibility
+    if(!window.Notification) {
+        // if browser version is unsupported, be silent
+        return;
+    }
+    // log current permission level
+    console.log(Notification.permissionLevel());
+    // if the user has not been asked to grant or deny notifications from this domain
+    if(Notification.permissionLevel() === 'default') {
+        Notification.requestPermission(function() {
+            // callback this function once a permission level has been set
+            notify();
+        });
+    }
+    // if the user has granted permission for this domain to send notifications
+    else if(Notification.permissionLevel() === 'granted') {
+        var n = new Notification(
+                    title,
+                    { 'body': body,
+                      // prevent duplicate notifications
+                      'tag' : token,
+                      // callback function when the notification is closed
+                      'onclose': function() {
+                           console.log('notification closed');
+                       }
+                    }
+                );
+    }
+    // if the user does not want notifications to come from this domain
+    else if(Notification.permissionLevel() === 'denied') {
+        // be silent
+        return;
+    }
+};
