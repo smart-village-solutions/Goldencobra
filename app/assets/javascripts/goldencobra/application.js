6089e6c86305e947dd73ef084cbe0e693d7a2ef2
ikusei Praktikant <praktikant@ikusei.de>
2013-01-04 17:08:40 +0100

    updated javascript for offline_times widgets

--- app/assets/javascripts/goldencobra/application.js
+++ app/assets/javascripts/goldencobra/application.js
@@ -16,45 +16,60 @@
 //= require goldencobra/jquery.tools.min.js
 //= require goldencobra/moment
 
-$(document).ready(function(){
+$(function(){
+  moment.weekdaysMin = ["su", "mo", "tu", "we", "th", "fr", "sa"];
+  
+  /* Trigger display of widgets. If widget has offline_times set, show
+     alternative content instead. */
+  $("[data-offline-active=true]").each(function(index, element) {
+    var currentDay = moment().format("dd");
+    var isCurrentDay = $(element).attr("data-time-day-" + currentDay) && $(element).attr("data-time-day-" + currentDay).length > 0 ? true : false;
+    
+    // wenn aktueller Wochentag gesetzt ist
+    if (isCurrentDay) {
+      // Zeitdaten
+      var currentTime = parseInt(moment().format("Hmm"));
+      var offTimeInterval = $(element).attr("data-time-day-" + currentDay).split("-");
+      var startTime = parseInt(offTimeInterval[0]);
+      var endTime = parseInt(offTimeInterval[1]);
+      // Zeitboolean
+      var isInTime = startTime <= currentTime && currentTime <= endTime ? true : false;
 
-  moment.weekdaysMin = ["So", "Mo", "Di", "Mi", "Do", "Fr", "Sa"];
-
-  // Trigger display of widgets. If widget has offline_times set show
-  // alternative content instead.
-  $('[data-offline-active=true]').each( function(index, element){
-    var days = $(element).attr('data-time-day');
-    var start_time = parseInt($(element).attr('data-time-start'));
-    var end_time = parseInt($(element).attr('data-time-end'));
-
-    var cur_day = moment().format('dd');
-    var cur_time = parseInt(moment().format('Hmm'));
-
-    var start_date = parseInt($(element).attr('data-date-start'));
-    var end_date = parseInt($(element).attr('data-date-end'));
-    var cur_date = parseInt(moment().format('YYYYMD'));
-
-    //Ausgabe wenn start und enddatum nicht gesetzt sind
-    if ($(element).attr('data-date-start').length == 0 && $(element).attr('data-date-end').length == 0){
-      if ((days.indexOf(cur_day) != -1) && ((start_time < cur_time) && (cur_time < end_time))) {
-        // Widget muss ausgeblendet werden
-        $(element).addClass("hidden");
-        $(element).next("[data-id="+ $(element).attr('data-id') + "]").removeClass("hidden");
-      }
-    }
-
-    //Ausgabe wenn start und enddatum gesetzt sind
-    if ($(element).attr('data-date-start').length > 0 && $(element).attr('data-date-end').length > 0 && start_date <= cur_date && end_date >= cur_date){
-      if ((days.indexOf(cur_day) != -1) && ((start_time < cur_time) && (cur_time < end_time))) {
-        // Widget muss ausgeblendet werden
-        $(element).addClass("hidden");
-        $(element).next("[data-id="+ $(element).attr('data-id') + "]").removeClass("hidden");
+      // wenn aktuelle Zeit im Intervall liegt
+      if (isInTime) {
+        // check, ob Start- und Endattribute vorhanden sind
+        var isDataDateStart = $(element).attr("data-date-start") && $(element).attr("data-date-start").length > 0 ? true : false;
+        var isDataDateEnd = $(element).attr("data-date-end") && $(element).attr("data-date-end").length > 0 ? true : false;
+        
+        /* wenn Start- und Enddatum nicht gesetzt sind,
+           dann muss der Offlineinhalt angezeigt werden */
+        if (!isDataDateStart && !isDataDateEnd) {
+          // Widget muss ausgeblendet werden
+          $(element).addClass("hidden");
+          $(element).next("[data-id=" + $(element).attr("data-id") + "]").removeClass("hidden");
+        }
+        /* Wenn Start- und Enddatum gesetzt sind,
+           muss geprüft werden, ob der aktuelle Tag
+           im Intervall liegt und wenn ja,
+           muss der Offlineinhalt angezeigt werden */
+        else if (isDataDateStart && isDataDateEnd) {
+          // Datumsdaten
+          var currentDate = parseInt(moment().format("YYYYMD"));
+          var startDate = parseInt($(element).attr("data-date-start"));
+          var endDate = parseInt($(element).attr("data-date-end"));
+          // Datumsboolean
+          var isCurrentDate = startDate <= currentDate && endDate >= currentDate ? true : false;
+          if (isCurrentDate) {
+            // Widget muss ausgeblendet werden
+            $(element).addClass("hidden");
+            $(element).next("[data-id=" + $(element).attr("data-id") + "]").removeClass("hidden");
+          }
+        }
       }
     }
   });
 
-  //console.log($('.goldencobra_widget.hidden').length);
-  $('.goldencobra_widget.hidden').remove();
-  //console.log($('.goldencobra_widget.hidden').length);
-
+  //console.log($(".goldencobra_widget.hidden").length);
+  $(".goldencobra_widget.hidden").remove();
+  //console.log($(".goldencobra_widget.hidden").length);
 });
