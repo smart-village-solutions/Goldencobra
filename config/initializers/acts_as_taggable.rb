87ad2301aed46712f11f2b0f7077cf90ea7411df
Marco Metz <marco.metz@gmail.com>
2013-01-10 11:54:09 +0100

    tagings erst wenn sie auch da asind

--- config/initializers/acts_as_taggable.rb
+++ config/initializers/acts_as_taggable.rb
@@ -1,2 +1,6 @@
-ActsAsTaggableOn.remove_unused_tags = true
-ActsAsTaggableOn.strict_case_match = true
+Rails.application.config.to_prepare do
+  if ActiveRecord::Base.connection.table_exists?("taggings")
+    ActsAsTaggableOn.remove_unused_tags = true
+    ActsAsTaggableOn.strict_case_match = true
+  end
+end
