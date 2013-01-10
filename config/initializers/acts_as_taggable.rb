27f0acbf095009680da2f433a3c3c84823893389
Marco Metz <marco.metz@gmail.com>
2013-01-10 12:12:48 +0100

    asd

--- config/initializers/acts_as_taggable.rb
+++ config/initializers/acts_as_taggable.rb
@@ -1,5 +1,5 @@
-# if ActsAsTaggableOn && ActsAsTaggableOn.method_defined?("strict_case_match")
-#   ActsAsTaggableOn.remove_unused_tags = true
-#   ActsAsTaggableOn.strict_case_match = true
-# end
+if ActsAsTaggableOn && ActsAsTaggableOn.method_defined?("strict_case_match")
+  ActsAsTaggableOn.remove_unused_tags = true
+  ActsAsTaggableOn.strict_case_match = true
+end
 
