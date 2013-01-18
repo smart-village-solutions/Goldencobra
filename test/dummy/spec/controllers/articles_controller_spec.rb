0327fc1678758a5f43b1816979d9f98dc16e7bfc
Holger Frohloff <holger.frohloff@gmail.com>
2013-01-18 18:00:30 +0100

    Abilities angepasst. Geht immer noch nciht.

--- test/dummy/spec/controllers/articles_controller_spec.rb
+++ test/dummy/spec/controllers/articles_controller_spec.rb
@@ -4,11 +4,10 @@ describe Goldencobra::ArticlesController do
   before(:each) { @routes = Goldencobra::Engine.routes }
   describe ".rss" do
     before(:each) do
-      @routes = Goldencobra::Engine.routes
       @article = Goldencobra::Article.create(title: "Mein Artikel", url_name: "mein-artikel")
     end
     it "should return an RSS feed" do
-      get :sitemap, format: "xml" #:show, :article_id => @article.url_name#, format: "rss"
+      get :sitemap, format: "xml"
       response.should be_success
     end
   end
@@ -16,7 +15,6 @@ describe Goldencobra::ArticlesController do
   describe  "permissions" do
     render_views
     before(:each) do
-      @routes = Goldencobra::Engine.routes
       @parent_article = create :article
       @child_article = create :article
       @user = create :user
@@ -25,49 +23,52 @@ describe Goldencobra::ArticlesController do
       @guest_role = create :role, :name => "Guest"
       @user.roles << @admin_role
       @visitor.roles << @guest_role
+      # create :permission, :action => "read", :subject_class => "Goldencobra::Article", :sorter_id => 1
     end
+
     it "should be possible to read an article if not logged in" do
       visit @parent_article.public_url
       page.should have_content(@parent_article.title)
     end
-    it "should be possible to read an article if not logged in" do
-      visit @parent_article.public_url
-      page.should have_content(@parent_article.title)
-    end
+
     it "should be possible to read an article as a visitor" do
       sign_in(:visitor, @visitor)
-      create :permission, :action => "read", :subject_class => "Goldencobra::Article", :subject_id => @parent_article.id.to_s, :role_id => @guest_role.id, :sorter_id => 10
       visit @parent_article.public_url
       page.should have_content(@parent_article.title)
     end
+
     it "should be possible to read an article as an admin" do
       sign_in(:user, @user)
-      create :permission, :action => "read", :subject_class => "Goldencobra::Article", :subject_id => @parent_article.id.to_s, :role_id => @admin_role.id, :sorter_id => 10
       visit @parent_article.public_url
       page.should have_content(@parent_article.title)
     end
-    it "should not be possible to read an secured article as an admin" do
+
+    it "should not be possible to read a secured article as an admin" do
+      create :permission, :action => "read", :subject_class => "Goldencobra::Article", :sorter_id => 1
+      create :permission, :action => "not_read", :subject_class => "Goldencobra::Article", :subject_id => @parent_article.id.to_s, :role_id => @admin_role.id, :sorter_id => 200
       sign_in(:user, @user)
-      create :permission, :action => "not_read", :subject_class => "Goldencobra::Article", :subject_id => @parent_article.id.to_s, :role_id => @admin_role.id, :sorter_id => 10
       visit @parent_article.public_url
-      Goldencobra::Permission.all.count.should == 1
       page.should have_content("You need to sign in or sign up before continuing")
     end
-    it "should not be possible to read an secured article as an visitor" do
+
+    it "should not be possible to read a secured article as a visitor" do
+      puts @guest_role.inspect
+      create :permission, :action => "not_read", :subject_class => "Goldencobra::Article", :role_id => @guest_role.id, :sorter_id => 200
+      create :permission, :action => "not_read", :subject_class => "Goldencobra::Article", :sorter_id => 200
       sign_in(:visitor, @visitor)
-      create :permission, :action => "not_read", :subject_class => "Goldencobra::Article", :subject_id => @parent_article.id.to_s, :role_id => @guest_role.id, :sorter_id => 10
       visit @parent_article.public_url
       page.should have_content("You need to sign in or sign up before continuing")
     end
-    it "should not be possible to read an article with secured parent article as an visitor" do
-      sign_in(:visitor, @visitor)
-      create :permission, :action => "not_read", :subject_class => "Goldencobra::Article", :subject_id => @parent_article.id.to_s, :role_id => @guest_role.id, :sorter_id => 10
-      visit @parent_article.public_url
-      page.should have_content("You need to sign in or sign up before continuing")
-    end
+
+    # it "should not be possible to read an article with secured parent article as an visitor" do
+    #   sign_in(:visitor, @visitor)
+    #   create :permission, :action => "not_read", :subject_class => "Goldencobra::Article", :role_id => @guest_role.id, :sorter_id => 200
+    #   visit @parent_article.public_url
+    #   page.should have_content("You need to sign in or sign up before continuing")
+    # end
+
     it "should not be possible to read secured an article if not logged in" do
-      # create :permission, :action => "read", :subject_class => "Goldencobra::Article", :sorter_id => 10
-      # create :permission, :action => "not_read", :subject_class => "Goldencobra::Article", :subject_id => @parent_article.id.to_s, :sorter_id => 9
+      create :permission, :action => "not_read", :subject_class => "Goldencobra::Article", :sorter_id => 200
       visit @parent_article.public_url
       page.should have_content("You need to sign in or sign up before continuing")
     end
