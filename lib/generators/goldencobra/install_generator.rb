b99d24fa45c03f94e7e32090619862f51cbb3edf
Marco Metz <marco.metz@gmail.com>
2013-01-24 11:09:59 +0100

    modified:   lib/generators/goldencobra/install_generator.rb

--- lib/generators/goldencobra/install_generator.rb
+++ lib/generators/goldencobra/install_generator.rb
@@ -47,8 +47,8 @@ module Goldencobra
       def install_assets
         require 'rails'
         require 'active_admin'
-        directory "../templates/assets", "app/"
-        directory "../templates/views", "app/"
+        directory "../templates/assets", "app/assets"
+        directory "../templates/views", "app/views"
         #template '../templates/active_admin.js', 'app/assets/javascripts/active_admin.js'
         #template '../templates/active_admin.css.scss', 'app/assets/stylesheets/active_admin.css.scss'
         template '../templates/extend_goldencobra_articles_controller.rb', 'app/controllers/extend_goldencobra_articles_controller.rb'
