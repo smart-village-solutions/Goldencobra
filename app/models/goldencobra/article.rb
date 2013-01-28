5f205e05451bf6e0487fcac25b1854fd08792a0c
Marco Metz <marco.metz@gmail.com>
2013-01-28 17:27:27 +0100

    modified:   app/models/goldencobra/article.rb

--- app/models/goldencobra/article.rb
+++ app/models/goldencobra/article.rb
@@ -98,6 +98,7 @@ module Goldencobra
     scope :parent_ids_in, lambda { |art_id| subtree_of(art_id) }
     scope :modified_since, lambda{ |date| where("updated_at > ?", Date.parse(date))}
     scope :for_sitemap, where('dynamic_redirection = "false" AND ( external_url_redirect IS NULL OR external_url_redirect = "") AND active = 1 AND robots_no_index =  0')
+    scope :frontend_tags_eq, lambda{|tag_name| tagged_with(tag_name, :on => :frontend_tags)}
 
     search_methods :parent_ids_in
     search_methods :parent_ids_in_eq
