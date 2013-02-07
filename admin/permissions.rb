46ec04b6d055e7b72fea85099919e8f803788c78
Marco Metz <marco.metz@gmail.com>
2013-02-07 12:14:11 +0100

    Select for Permission actions

--- admin/permissions.rb
+++ admin/permissions.rb
@@ -25,7 +25,7 @@ ActiveAdmin.register Goldencobra::Permission, :as => "Permission", :sort_order =
       f.actions
       f.inputs do
         f.input :role_id, as: :select, collection: Goldencobra::Role.all.map{|role| [role.name.capitalize, role.id]}, include_blank: false
-        f.input :action
+        f.input :action, :as => :select, :collection => Goldencobra::Permission::PossibleActions, :include_blank => false
         f.input :subject_class, as: :select, collection: Goldencobra::Permission::PossibleSubjectClasses, include_blank: false
         f.input :subject_id
         f.input :sorter_id
