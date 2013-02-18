3fc701888eb64b0dd4788dd4507cfe5ba8e836fc
Holger Frohloff <holger.frohloff@gmail.com>
2013-02-18 11:20:28 +0100

    Module für API tokens

--- app/controllers/goldencobra/api/v1/tokens_controller.rb
+++ app/controllers/goldencobra/api/v1/tokens_controller.rb
@@ -1,31 +1,35 @@
 module Goldencobra
-  class Api::V1::TokensController < ActionController::Base
+  module Api
+    module V1
+      class TokensController < ActionController::Base
 
-    respond_to :json, :xml
+        respond_to :json, :xml
 
-    def create
-      email = params[:email]
-      password = params[:password]
+        def create
+          email = params[:email]
+          password = params[:password]
 
-      if email.nil? || password.nil?
-        render status: 400, json: { error: 'The request must contain ' +
-                                        'the user email and password.' }
-        return
-      end
+          if email.nil? || password.nil?
+            render status: 400, json: { error: 'The request must contain ' +
+                                            'the user email and password.' }
+            return
+          end
 
-      @visitor = Visitor.find_by_email(email)
+          @visitor = Visitor.find_by_email(email)
 
-      if @visitor == nil
-        render status: 401, json: { error: "Invalid email or password." }
-        return
-      end
+          if @visitor == nil
+            render status: 401, json: { error: "Invalid email or password." }
+            return
+          end
 
-      @visitor.ensure_authentication_token!
+          @visitor.ensure_authentication_token!
 
-      if @visitor.valid_password?(password)
-        render status: 200, json: { token: @visitor.authentication_token }
-      else
-        render status: 401, json: { error: "Invalid email or password." }
+          if @visitor.valid_password?(password)
+            render status: 200, json: { token: @visitor.authentication_token }
+          else
+            render status: 401, json: { error: "Invalid email or password." }
+          end
+        end
       end
     end
   end
