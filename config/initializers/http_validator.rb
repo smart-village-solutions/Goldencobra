ad64a7f99a80a464a1fc83ad2d526e9a10751a16
Marco Metz <marco.metz@gmail.com>
2013-02-25 10:43:02 +0100

    rspec up and running again

--- config/initializers/http_validator.rb
+++ config/initializers/http_validator.rb
@@ -1,21 +1,21 @@
 module HttpValidator
-  def web_url(*attr_names)  
+  def web_url(*attr_names)
     include InstanceMethods
     cattr_accessor :url_attribute_names_to_check
     self.url_attribute_names_to_check = attr_names
     before_save :rewrite_web_url
   end
-  
+
   module InstanceMethods
     def rewrite_web_url
       self.url_attribute_names_to_check.each do |attr_name|
         if self.respond_to?(attr_name) && self.send(attr_name).present? && self.send(attr_name)[0..6] != "http://"
-          self.send("#{attr_name.to_s}=", "http://#{self.send(attr_name)}")  
+          self.send("#{attr_name.to_s}=", "http://#{self.send(attr_name)}")
         end
       end
     end
   end
-  
+
 end
 
 ActiveRecord::Base.extend HttpValidator
