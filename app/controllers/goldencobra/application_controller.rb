18e6bc773d69604f62a7a37c6e5354e08fe9b019
Marco Metz <marco.metz@gmail.com>
2013-03-03 14:45:58 +0100

    title tag

--- app/controllers/goldencobra/application_controller.rb
+++ app/controllers/goldencobra/application_controller.rb
@@ -33,7 +33,7 @@ module Goldencobra
 
     def initialize_article(current_article)
       Goldencobra::Article::LiquidParser["current_article"] = @article
-      set_meta_tags :site => s("goldencobra.page.default_title_tag"),
+      set_meta_tags :site => "#{@article.metatag("Title Tag") || @article.title} | #{s('goldencobra.page.default_title_tag')}",
                     :title => current_article.metatag("Title Tag"),
                     :description => current_article.metatag("Meta Description"),
                     :keywords => current_article.metatag("Keywords"),
