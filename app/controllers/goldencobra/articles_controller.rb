9343c6f41d9561f48659c0c72990e8c1109f6331
Marco Metz <marco.metz@gmail.com>
2013-03-04 21:40:17 +0100

    modified:   app/controllers/goldencobra/articles_controller.rb

--- app/controllers/goldencobra/articles_controller.rb
+++ app/controllers/goldencobra/articles_controller.rb
@@ -245,6 +245,8 @@ module Goldencobra
       if I18n.available_locales.include?(locale_article.to_sym)
         I18n.locale = locale_article
         session[:locale] = I18n.locale
+      else
+        I18n.locale = session[:locale]
       end
     end
 
