f95b1b2d1c0d5f534bc70e7d6a8315a20e5d6503
Marco Metz <marco.metz@gmail.com>
2013-03-07 16:18:48 +0100

    modified:   admin/permissions.rb 	modified:   app/models/goldencobra/permission.rb

--- app/models/goldencobra/permission.rb
+++ app/models/goldencobra/permission.rb
@@ -16,7 +16,7 @@ module Goldencobra
   class Permission < ActiveRecord::Base
     attr_accessible :role_id, :action, :subject_class, :subject_id, :sorter_id
     belongs_to :role
-    PossibleSubjectClasses = [":all"] + ActiveRecord::Base.descendants.map(&:name)
+    PossibleSubjectClasses = [":all"] + ActiveRecord::Base.descendants.map(&:name) + ["Test"]
     PossibleActions = ["read", "not_read", "manage", "not_manage", "update", "not_update", "destroy", "not_destroy"]
 
     default_scope order("sorter_id ASC, id")
