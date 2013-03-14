8bf0c4d440ef78371485b85e6c6e9e3bfd2c9b7b
Marco Metz <marco.metz@gmail.com>
2013-03-14 12:30:38 +0100

    modified:   app/helpers/goldencobra/login_helper.rb 	modified:   app/views/goldencobra/sessions/_register.html.erb 	modified:   test/dummy/app/views/layouts/application.html.erb

--- app/helpers/goldencobra/login_helper.rb
+++ app/helpers/goldencobra/login_helper.rb
@@ -5,8 +5,8 @@ module Goldencobra
       render :partial => "/goldencobra/sessions/login", :locals => {:usermodel => usermodel_name}
     end
 
-    def render_registration_widget(usermodel_name)
-      render :partial => "/goldencobra/sessions/register", :locals => {:usermodel => usermodel_name}
+    def render_registration_widget(usermodel_name, associated_model=nil)
+      render :partial => "/goldencobra/sessions/register", :locals => {:usermodel => usermodel_name, :associated_model => associated_model}
     end
 
   end
