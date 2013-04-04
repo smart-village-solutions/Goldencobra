d7c23515f3cae4a7528f4a91947d9512031fb21c
Marco Metz <marco.metz@gmail.com>
2013-04-04 16:54:05 +0200

    trackings ins model verlagert

--- app/controllers/goldencobra/articles_controller.rb
+++ app/controllers/goldencobra/articles_controller.rb
@@ -333,9 +333,7 @@ module Goldencobra
     end
 
     def analytics
-      if Goldencobra::Setting.for_key("goldencobra.analytics.active") == "true"
-        Goldencobra::Tracking.create(:language => request.env["HTTP_ACCEPT_LANGUAGE"], :user_agent => request.env["HTTP_USER_AGENT"], :session_id => request.session_options[:id], :referer => request.referer, :url => request.url, :ip => request.env['REMOTE_ADDR'] )
-      end
+      Goldencobra::Tracking.analytics(request)
     end
 
 
--- app/models/goldencobra/tracking.rb
+++ app/models/goldencobra/tracking.rb
@@ -1,5 +1,12 @@
 module Goldencobra
   class Tracking < ActiveRecord::Base
     attr_accessible :ip, :referer, :request, :session_id, :url, :user_agent, :language
+
+    def self.analytics(request)
+      if Goldencobra::Setting.for_key("goldencobra.analytics.active") == "true"
+        self.create!(:language => request.env["HTTP_ACCEPT_LANGUAGE"], :user_agent => request.env["HTTP_USER_AGENT"], :session_id => request.session_options[:id], :referer => request.referer, :url => request.url, :ip => request.env['REMOTE_ADDR'] )
+      end
+    end
+
   end
 end
