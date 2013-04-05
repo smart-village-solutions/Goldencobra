3e0115f4fbd1bb51a05e8c637ae66c9eacbbcbac
Marco Metz <marco.metz@gmail.com>
2013-04-05 09:51:21 +0200

    location tracking

--- admin/trackings.rb
+++ admin/trackings.rb
@@ -9,15 +9,28 @@ ActiveAdmin.register Goldencobra::Tracking, :as => "Analytic" do
   filter :language
   filter :user_agent
   filter :url_paremeters
+  filter :utm_source
+  filter :utm_medium
+  filter :utm_term
+  filter :utm_content
+  filter :utm_campaign
+  filter :location
   filter :created_at
 
   index do
-    column :ip
+    column :utm_campaign
     column :session_id
-    column :referer
-    column :url
+    column :referer, :sortable => :referer do |tracking|
+      truncate(tracking.referer, :length => 50)
+    end
+    column :url, :sortable => :url do |tracking|
+      truncate(tracking.url, :length => 50)
+    end
     column :language
-    column :created_at
+    column :location
+    column :created_at, :sortable => :created_at do |tracking|
+      raw(tracking.created_at.strftime("%d.%m.%y&nbsp;%H:%M"))
+    end
     default_actions
   end
 
