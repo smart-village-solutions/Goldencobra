e46766c622879805c166379aa25f9d55c903b936
Marco Metz <marco.metz@gmail.com>
2013-05-21 15:41:31 +0200

    Default Text meta

--- app/models/goldencobra/tracking.rb
+++ app/models/goldencobra/tracking.rb
@@ -1,3 +1,30 @@
+# == Schema Information
+#
+# Table name: goldencobra_trackings
+#
+#  id             :integer          not null, primary key
+#  request        :text
+#  session_id     :string(255)
+#  referer        :string(255)
+#  url            :string(255)
+#  ip             :string(255)
+#  created_at     :datetime         not null
+#  updated_at     :datetime         not null
+#  user_agent     :string(255)
+#  language       :string(255)
+#  path           :string(255)
+#  page_duration  :string(255)
+#  view_duration  :string(255)
+#  db_duration    :string(255)
+#  url_paremeters :string(255)
+#  utm_source     :string(255)
+#  utm_medium     :string(255)
+#  utm_term       :string(255)
+#  utm_content    :string(255)
+#  utm_campaign   :string(255)
+#  location       :string(255)
+#
+
 module Goldencobra
   class Tracking < ActiveRecord::Base
     attr_accessible :ip, :referer, :request, :session_id, :url, :user_agent, :language, :url_paremeters, :path, :page_duration, :view_duration, :db_duration
--- app/models/translation.rb
+++ app/models/translation.rb
@@ -1,3 +1,17 @@
+# == Schema Information
+#
+# Table name: translations
+#
+#  id             :integer          not null, primary key
+#  locale         :string(255)
+#  key            :string(255)
+#  value          :text
+#  interpolations :text
+#  is_proc        :boolean          default(FALSE)
+#  created_at     :datetime         not null
+#  updated_at     :datetime         not null
+#
+
 class Translation < ActiveRecord::Base
   attr_accessible :interpolations, :is_proc, :key, :locale, :value
 
