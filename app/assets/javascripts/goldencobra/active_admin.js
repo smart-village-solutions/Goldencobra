23c05212a4f21ed329678d2069a54993d7d26afc
Philipp Wilimzig <wilimzig@ikusei.de>
2013-06-02 18:58:22 +0200

    erweiterter TinyMCE per class=tinymce_extended

--- app/assets/javascripts/goldencobra/active_admin.js
+++ app/assets/javascripts/goldencobra/active_admin.js
@@ -20,6 +20,24 @@ $(function() {
 		paste_auto_cleanup_on_paste : true
 	});
 
+	$('textarea.tinymce_extended').tinymce({
+		script_url: "/assets/goldencobra/tiny_mce.js",
+  		mode : "textareas",
+  		theme : "advanced",
+  		theme_advanced_buttons1 : "formatselect, bold, italic, underline, strikethrough,|, bullist, numlist, blockquote, |, pastetext,pasteword, |, undo, redo, |, link, unlink, code, fullscreen",
+  		theme_advanced_buttons2 : "removeformat, fontsizeselect, forecolor, backcolor, forecolorpicker, backcolorpicker",
+  		theme_advanced_buttons3 : "tablecontrols",
+  		theme_advanced_toolbar_location : "top",
+  		theme_advanced_toolbar_align : "center",
+  		theme_advanced_resizing : false,
+		relative_urls : true,
+    convert_urls : false,
+		theme_advanced_blockformats : "p,h1,h2,h3,div",
+		plugins : "fullscreen,autolink,paste,table",
+		dialog_type : "modal",
+		paste_auto_cleanup_on_paste : true
+	});
+
 
   //Foldable overview in sidebar
   $("div.overview-sidebar div.folder").bind("click", function(){
