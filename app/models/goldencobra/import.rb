ad219095ed999faaf8e54de71298ae770ec0f294
Marco Metz <marco.metz@gmail.com>
2013-07-23 12:53:19 +0200

    asdf

--- app/models/goldencobra/import.rb
+++ app/models/goldencobra/import.rb
@@ -2,15 +2,16 @@
 #
 # Table name: goldencobra_imports
 #
-#  id           :integer          not null, primary key
-#  assignment   :text
-#  target_model :string(255)
-#  successful   :boolean
-#  upload_id    :integer
-#  separator    :string(255)      default(",")
-#  result       :text
-#  created_at   :datetime         not null
-#  updated_at   :datetime         not null
+#  id                :integer          not null, primary key
+#  assignment        :text
+#  assignment_groups :text
+#  target_model      :string(255)
+#  successful        :boolean
+#  upload_id         :integer
+#  separator         :string(255)      default(",")
+#  result            :text
+#  created_at        :datetime         not null
+#  updated_at        :datetime         not null
 #
 
 module Goldencobra
@@ -20,16 +21,21 @@ module Goldencobra
 
     belongs_to :upload, :class_name => Goldencobra::Upload
     serialize :assignment
+    serialize :assignment_groups
     EncodingTypes = ["UTF-8","ISO-8859-1", "US-ASCII", "Big5", "UTF-16BE", "IBM437", "Windows-1252"]
     accepts_nested_attributes_for :upload, :allow_destroy => true, :reject_if => proc { |attributes| attributes['image'].blank? }
     after_initialize :init_nested_attributes
-    BlockedAttributes = ["id", "created_at", "updated_at", "url_name", "slug"]
-
+    BlockedAttributes = ["id", "created_at", "updated_at", "url_name", "slug", "upload_id", "images"]
+    DataHandling = [["Datensatz aktualisieren","update"],["Datensatz neu anlegen", "create"]]
     def analyze_csv
-      result = []
-      data = CSV.read(self.upload.image.path, "r:#{self.encoding_type}", {:col_sep => self.separator})
-      data.first.each_with_index do |a, index|
-        result << [a,index.to_s]
+      begin
+        result = []
+        data = CSV.read(self.upload.image.path, "r:#{self.encoding_type}", {:col_sep => self.separator})
+        data.first.each_with_index do |a, index|
+          result << [a,index.to_s]
+        end
+      rescue
+        result = []
       end
       @analyze_csv ||= result
     end
@@ -39,7 +45,7 @@ module Goldencobra
     end
 
     def get_association_names
-      self.target_model.constantize.reflect_on_all_associations.collect { |r| r.name }
+      self.target_model.constantize.reflect_on_all_associations.collect { |r| r.name }.delete_if{|a| Goldencobra::Import::BlockedAttributes.include?(a.to_s) }
     end
 
     def method_missing(meth, *args, &block)
