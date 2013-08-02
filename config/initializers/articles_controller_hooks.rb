91ca41a75a3518b9b4a605386026e461feaeb8d1
Marco Metz <marco.metz@gmail.com>
2013-08-02 15:42:07 +0200

    exended module

--- config/initializers/articles_controller_hooks.rb
+++ config/initializers/articles_controller_hooks.rb
@@ -3,6 +3,11 @@ Rails.application.config.to_prepare do
     t = File.open(File.join(Goldencobra::Engine.root,'lib/generators/goldencobra/templates/extend_goldencobra_articles_controller.rb'), "r")
     f = File.open(File.join(::Rails.root, 'app/controllers/extend_goldencobra_articles_controller.rb'), "w+") {|f| f.write(t.read)}
   end
+  begin
+  	ExtendGoldencobraArticlesController
+  rescue
+
+  end
   if defined?(ExtendGoldencobraArticlesController) == 'constant' && ExtendGoldencobraArticlesController.class == Module
     Goldencobra::ArticlesController.class_eval { include ExtendGoldencobraArticlesController  }
   end
