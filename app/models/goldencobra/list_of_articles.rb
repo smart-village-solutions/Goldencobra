69c41e3d2d8396d17fb27343fcbbb13c1de5c325
Holger Frohloff <holger.frohloff@kircher-burkhardt.com>
2013-08-20 13:18:03 +0200

    Refactoring article.rb. Extracted methods to new classes.
    Created Goldencobra::ArticleType and Goldencobra::ListOfArticles to
    comply with the Single Responsibility Principle. Goldencobra::Article
    isn't responsible for List_of_articles and article_type anymore.

--- app/models/goldencobra/list_of_articles.rb
+++ app/models/goldencobra/list_of_articles.rb
@@ -4,6 +4,7 @@ module Goldencobra
   
     def initialize(article, current_operator=nil, user_frontend_tags=nil)
       @article = article
+      @current_operator = current_operator
       @list_of_articles = []
 
       #Index aller Artikel anzeigen, die Kinder sind von einem Bestimmten artikel
@@ -16,9 +17,9 @@ module Goldencobra
 
       include_related_models
 
-      consider_tagging
+      consider_tagging(user_frontend_tags)
 
-      filter_with_permissions(current_operator)
+      filter_with_permissions
 
       sort_list
   
@@ -39,7 +40,7 @@ module Goldencobra
       @list_of_articles = @list_of_articles.includes("#{@article.article_type_form_file.downcase}") if @article.respond_to?(@article.article_type_form_file.downcase)
     end
 
-    def consider_tagging
+    def consider_tagging(user_frontend_tags)
       #get articles with tag
       if @article.index_of_articles_tagged_with.present?
         @list_of_articles = @list_of_articles.tagged_with(@article.index_of_articles_tagged_with.split(",").map{|t| t.strip}, on: :tags, any: true)
@@ -55,15 +56,15 @@ module Goldencobra
     end
 
     def current_operator_can_preview_all_articles
-      current_operator && current_operator.respond_to?(:has_role?) &&
-       current_operator.has_role?(Goldencobra::Setting.for_key("goldencobra.article.preview.roles").split(",").map{|a| a.strip})
+      @current_operator && @current_operator.respond_to?(:has_role?) &&
+       @current_operator.has_role?(Goldencobra::Setting.for_key("goldencobra.article.preview.roles").split(",").map{|a| a.strip})
     end
 
     # Methode filtert die @list_of_articles.
     # Rückgabewert: Ein Array all der Artikel, die der operator lesen darf.
-    def filter_with_permissions(current_operator)
+    def filter_with_permissions
       unless current_operator_can_preview_all_articles
-        a = Ability.new(current_operator)
+        a = Ability.new(@current_operator)
         new_list = []
         @list_of_articles.each do |article|
           if a.can?(:read, article)
@@ -98,22 +99,22 @@ module Goldencobra
         @list_of_articles = @list_of_articles[0...@article.sorter_limit]
       end
     end
-  end
-
-  def sort_by_article_attribute
-    if @article.respond_to?(@article.sort_order)
-      sort_order = @article.sort_order.downcase
-      @list_of_articles = @list_of_articles.flatten.sort_by{|art| art.respond_to?(sort_order) ? art.send(sort_order) : art }
+    
+    def sort_by_article_attribute
+      if @article.respond_to?(@article.sort_order)
+        sort_order = @article.sort_order.downcase
+        @list_of_articles = @list_of_articles.flatten.sort_by{|art| art.respond_to?(sort_order) ? art.send(sort_order) : art }
+      end
     end
-  end
 
-  def sort_by_related_object_attribute
-    @unsortable = @list_of_articles.flatten.select{|a| !a.respond_to_all?(@article.sort_order) }
-    @list_of_articles = @list_of_articles.flatten.delete_if{|a| !a.respond_to_all?(@article.sort_order) }
-    @list_of_articles = @list_of_articles.sort_by{|a| eval("a.#{@article.sort_order}") }
-    if @unsortable.any?
-      @list_of_articles = @unsortable + @list_of_articles
-      @list_of_articles = @list_of_articles.flatten
+    def sort_by_related_object_attribute
+      @unsortable = @list_of_articles.flatten.select{|a| !a.respond_to_all?(@article.sort_order) }
+      @list_of_articles = @list_of_articles.flatten.delete_if{|a| !a.respond_to_all?(@article.sort_order) }
+      @list_of_articles = @list_of_articles.sort_by{|a| eval("a.#{@article.sort_order}") }
+      if @unsortable.any?
+        @list_of_articles = @unsortable + @list_of_articles
+        @list_of_articles = @list_of_articles.flatten
+      end
     end
   end
 end
