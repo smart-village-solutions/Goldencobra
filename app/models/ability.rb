38f04a43758c624099e617efb2c23acf65319c4b
Holger Frohloff <holger.frohloff@kircher-burkhardt.com>
2013-08-21 13:04:19 +0200

    Refactored ability.rb for readability

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -8,21 +8,7 @@ class Ability
 
     #Rechte die alle betreffen
     Goldencobra::Permission.where("action IS NOT NULL").where("role_id IS NULL OR role_id = ''").each do |permission|
-      if permission.subject_id.blank?
-        if permission.action.include?("not_")
-          cannot permission.action.gsub("not_", "").to_sym, permission.subject_class.constantize
-        else
-          can permission.action.to_sym, permission.subject_class.constantize
-        end
-      else
-        if permission.action.include?("not_")
-          cannot permission.action.gsub("not_", "").to_sym, permission.subject_class.constantize, :id => permission.subject_id.to_i
-          set_child_permissions("cannot",permission.action.gsub("not_", "").to_sym, permission.subject_class.constantize,permission.subject_id.to_i)
-        else
-          can permission.action.to_sym, permission.subject_class.constantize, :id => permission.subject_id.to_i
-          set_child_permissions("can",permission.action.to_sym, permission.subject_class.constantize,permission.subject_id.to_i)
-        end
-      end
+      set_permission(permission)
     end
 
     #Rechte, die nur bestimmte nutzerrollen betreffen
@@ -36,43 +22,45 @@ class Ability
             else
               can permission.action.to_sym, :all
             end
-          end
-
-          if !permission.subject_class.include?(":all") && permission.subject_id.blank?
-            if permission.action.include?("not_")
-              cannot permission.action.gsub("not_", "").to_sym, permission.subject_class.constantize
-            else
-              can permission.action.to_sym, permission.subject_class.constantize
-            end
-          end
-
-          if !permission.subject_class.include?(":all") && permission.subject_id.present?
-            if permission.action.include?("not_")
-              cannot permission.action.to_s.gsub("not_", "").to_sym, permission.subject_class.constantize, :id => permission.subject_id.to_i
-              set_child_permissions("cannot",permission.action.gsub("not_", "").to_sym, permission.subject_class.constantize,permission.subject_id.to_i)
-            else
-              can permission.action.to_sym, permission.subject_class.constantize, :id => permission.subject_id.to_i
-              set_child_permissions("can",permission.action.to_sym, permission.subject_class.constantize,permission.subject_id.to_i)
-            end
+          else
+            set_permission(permission)
           end
         end
       end
     end
-
   end
 
-  def set_child_permissions(able,action_name, model_name,id_name)
-    if able == "can" && model_name.new.respond_to?(:descendant_ids) && object = model_name.find_by_id(id_name)
-      object.descendant_ids.each do |child_id|
-        can action_name, model_name, :id => child_id
-      end
-    end
-    if able == "cannot" && model_name.new.respond_to?(:descendant_ids) && object = model_name.find_by_id(id_name)
-      object.descendant_ids.each do |child_id|
-        cannot action_name, model_name, :id => child_id
-      end
+  def set_permission(permission)
+    able = 'can'
+    action_name = permission.action.to_sym
+    model_name = permission.subject_class.constantize
+
+    if permission.action.include?("not_")
+      action_name = permission.action.gsub("not_", "").to_sym
+      able = 'cannot'
     end
 
+    if permission.subject_id.present?
+      set_ability(able, action_name, model_name, permission.subject_id.to_i)
+      set_child_permissions(able, action_name, model_name, permission.subject_id.to_i)
+    else
+      set_ability(able, action_name, model_name)
+    end
   end
 
+  def set_ability(able, action_name, model_name, id_name=nil)
+    if id_name.present?
+      send(able, action_name, model_name, id: id_name)
+    else
+      send(able, action_name, model_name)
+    end
+  end
+
+  def set_child_permissions(able, action_name, model_name,id_name)
+    if %w(can cannot).include?(able) && model_name.new.respond_to?(:descendant_ids) && object = model_name.find_by_id(id_name)
+      object.descendant_ids.each do |child_id|
+        set_ability(able, action_name, model_name, child_id)
+      end
+    end
+  end
 end
