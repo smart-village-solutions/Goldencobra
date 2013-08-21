b589c4bf48b3a3d97a1a854b183c6f1d2d670177
Holger Frohloff <holger.frohloff@kircher-burkhardt.com>
2013-08-21 17:10:51 +0200

    Optimized metatag for simplicity

--- app/models/goldencobra/metatag.rb
+++ app/models/goldencobra/metatag.rb
@@ -19,27 +19,20 @@ module Goldencobra
     belongs_to :metatagable, polymorphic: true
 
     def self.set_default_opengraph_values(article)
-      if Goldencobra::Metatag.where(article_id: article.id, name: 'OpenGraph Title').none?
-        create_tag('OpenGraph Title', article.id, article.title)
-      end
+      create_tag('OpenGraph Title', article.id, article.title)
+      create_tag('OpenGraph URL', article.id, article.absolute_public_url)
 
-      if Goldencobra::Metatag.where(article_id: article.id, name: 'OpenGraph URL').none?
-        create_tag('OpenGraph URL', article.id, article.absolute_public_url)
+      if article.teaser.present?
+        value = article.teaser
+      else
+        value = article.content.present? ? article.content.truncate(200) : article.title
       end
-
-      if Goldencobra::Metatag.where(article_id: article.id, name: 'OpenGraph Description').none?
-        if article.teaser.present?
-          value = article.teaser
-        else
-          value = article.content.present? ? article.content.truncate(200) : article.title
-        end
-        create_tag('OpenGraph Description', article.id, value)
-      end
+      create_tag('OpenGraph Description', article.id, value)
     end
 
     def self.verify_existence_of_opengraph_image(article)
-      if Goldencobra::Metatag.where("article_id = ? AND name = 'OpenGraph Image'", article.id).count == 0
-        if article.article_images.any? && article.article_images.first.image.present? && article.article_images.first.image.image.present?
+      unless tag_exists?(article.id, 'OpenGraph Image')
+        if article_has_image?(article)
           meta_tag = Goldencobra::Metatag.where(article_id: article.id, name: "OpenGraph Image").first
           meta_tag.value = "http://#{Goldencobra::Setting.for_key('goldencobra.url')}#{article.article_images.first.image.image.url}"
           meta_tag.save
@@ -51,7 +44,17 @@ module Goldencobra
 
     private
     def self.create_tag(name, id, value)
-      Goldencobra::Metatag.create(name: name, article_id: id, value: value)
+      unless tag_exists?(id, name)
+        Goldencobra::Metatag.create(name: name, article_id: id, value: value)
+      end
+    end
+
+    def self.article_has_image?(article)
+      article.article_images.any? && article.article_images.first.image.present? && article.article_images.first.image.image.present?
+    end
+
+    def self.tag_exists?(article_id, name)
+      Goldencobra::Metatag.where(article_id: article_id, name: name).any?
     end
   end
 end
