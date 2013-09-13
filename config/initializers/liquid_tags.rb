3b6acf3d247fb00cb3995706c402775a5ab30f97
Marco Metz <marco.metz@gmail.com>
2013-09-13 11:34:18 +0200

    asdf

--- config/initializers/liquid_tags.rb
+++ config/initializers/liquid_tags.rb
@@ -7,7 +7,7 @@ class PartialRenderer < Liquid::Tag
   end
 
   def render(context)
-      ActionController::Base.new.render_to_string(:partial => @message.strip, :layout => false, :context => context)
+      ActionController::Base.new.render_to_string(:partial => @message.strip, :layout => false, :locals => {:context => context})
   end
 end
 
