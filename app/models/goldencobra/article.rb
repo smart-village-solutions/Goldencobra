2ab464121cc94916d4ba781ea57223490f6a05ea
Marco Metz <marco.metz@gmail.com>
2013-10-04 17:31:40 +0200

    link_checker mit position

--- app/models/goldencobra/article.rb
+++ app/models/goldencobra/article.rb
@@ -180,23 +180,27 @@ module Goldencobra
       doc = Nokogiri::HTML(open(self.absolute_public_url))
       #find all links and stylesheets
       doc.css('a,link').each do |link|
-        links_to_check << add_link_to_checklist(link, "href")
+        if add_link_to_checklist(link, "href").present?
+          links_to_check << {"link" => add_link_to_checklist(link, "href"), "pos" => link.path}
+        end
       end
       #find all images and javascripts
       doc.css('img,script').each do |link|
-        links_to_check << add_link_to_checklist(link,"src")
+        if add_link_to_checklist(link,"src").present?
+          links_to_check << {"link" => add_link_to_checklist(link,"src"), "pos" => link.path}
+        end
       end
       links_to_check = links_to_check.compact.delete_if{|a| a.blank?}
-      links_to_check.each_with_index do |link|
-        status_for_links[link] = {}
+      links_to_check.each_with_index do |linkpos|
+        status_for_links[linkpos["link"]] = {"position" => linkpos["pos"]}
         begin
           start = Time.now
-          response = open(link)
-          status_for_links[link]["response_code"] = response.status[0]
-          status_for_links[link]["response_time"] = Time.now - start
+          response = open(linkpos["link"])
+          status_for_links[linkpos["link"]]["response_code"] = response.status[0]
+          status_for_links[linkpos["link"]]["response_time"] = Time.now - start
         rescue Exception  => e
-          status_for_links[link]["response_code"] = "404"
-          status_for_links[link]["response_error"] = e.to_s
+          status_for_links[linkpos["link"]]["response_code"] = "404"
+          status_for_links[linkpos["link"]]["response_error"] = e.to_s
         end
       end
       self.link_checker = status_for_links
