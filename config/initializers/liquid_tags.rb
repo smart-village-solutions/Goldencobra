147dbec3e5b81e9da3b49f68480cb9428527cf19
Daniel Molnar <dm@ikusei.de>
2013-10-23 15:02:15 +0200

    kleine textaenderung

--- config/initializers/liquid_tags.rb
+++ config/initializers/liquid_tags.rb
@@ -2,12 +2,12 @@ class PartialRenderer < Liquid::Tag
   #include ActionController::RequestForgeryProtection
 
   def initialize(tag_name, message, tokens)
-       super
-       @message = message
+    super
+    @message = message
   end
 
   def render(context)
-      ActionController::Base.new.render_to_string(:partial => @message.strip, :layout => false, :locals => {:context => context})
+    ActionController::Base.new.render_to_string(:partial => @message.strip, :layout => false, :locals => {:context => context})
   end
 end
 
