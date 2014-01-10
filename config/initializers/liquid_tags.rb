6445c271c366ebcde483afaaff4dc41fe04ac96d
Marco Metz <marco.metz@gmail.com>
2014-01-10 13:42:14 +0100

    domain liquid helper

--- config/initializers/liquid_tags.rb
+++ config/initializers/liquid_tags.rb
@@ -1,3 +1,5 @@
+# encoding: utf-8
+
 class PartialRenderer < Liquid::Tag
   #include ActionController::RequestForgeryProtection
   def self.description
@@ -27,6 +29,34 @@ end
 Liquid::Template.register_tag('render_partial', PartialRenderer)
 
 
+
+
+class DomainUrl < Liquid::Tag
+  #include ActionController::RequestForgeryProtection
+  def self.description
+    "gibt die aktuelle domain mit url prefix aus"
+  end
+
+  def self.usage
+    "{% domain %} => www.xyz.de/url_prefix"
+  end
+
+  def initialize(tag_name, message, tokens)
+    super
+    @partial_name = message.split("|")[0].to_s.strip
+    if message.split('|')[1].present? && message.split('|')[1].include?(":")
+      @options = message.split('|')[1].split(', ').map{ |h| h1, h2 = h.split(":"); { h1.strip.to_sym => h2.strip } }.reduce(:merge)
+    else
+      @options = {}
+    end
+  end
+
+  def render(context)
+    @options = @options.merge(:context => context)
+    ActionController::Base.new.render_to_string(:partial => @partial_name, :layout => false, :locals => @options )
+  end
+end
+Liquid::Template.register_tag('domain', DomainUrl)
 
 # {% navigation_menue 3 | id: css_id_name, class_name: test %}
 class NavigationRenderer < Liquid::Tag
