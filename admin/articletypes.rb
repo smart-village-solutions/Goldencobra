59820fd23b8e2d2be4cd8c037d90b84d03e8da6d
Marco Metz <marco.metz@gmail.com>
2014-01-28 13:54:00 +0100

    layouts and helps

--- admin/articletypes.rb
+++ admin/articletypes.rb
@@ -18,14 +18,14 @@ ActiveAdmin.register Goldencobra::Articletype, :as => "Articletype" do
     f.inputs "Artikel Felder", :class => "foldable closed inputs" do
       f.has_many :fieldgroups do |fg|
         fg.input :title
-        fg.input :position, :as => :select, :collection => [["First Block","first_block"],["Last Block","last_block"]], :include_blank => false
+        fg.input :position, :as => :select, :collection => [["First Block","first_block"],["Last Block","last_block"]], :include_blank => false, :hint => "Zwischen dem ersten und dem letzten Block kommen die speziellen Feldoptionen eines Articeltypen"
         fg.input :foldable, :hint => "Kann man den Bereich auf und zu klappen?"
         fg.input :closed, :hint => "Ist der Bereich beim laden geöffnet oder geschlossen"
         fg.input :expert, :hint => "Ist der Bereich nur im Expertenmodus sichtbar"
         fg.input :sorter, :hint => "Sortiernummer/Position"
         fg.input :_destroy, :as => :boolean
         fg.has_many :fields do |fo|
-          fo.input :fieldname, :as => :select, :collection => Goldencobra::Articletype::ArticleFieldOptions.keys
+          fo.input :fieldname, :as => :select, :collection => Goldencobra::Articletype::ArticleFieldOptions.keys, :include_blank => false
           fo.input :sorter
           fo.input :_destroy, :as => :boolean
         end
