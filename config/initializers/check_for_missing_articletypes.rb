2758551951e71d218d404965027072db3d8634a4
Marco Metz <marco.metz@gmail.com>
2014-01-30 09:48:28 +0100

    regenerate articletypes

--- config/initializers/check_for_missing_articletypes.rb
+++ config/initializers/check_for_missing_articletypes.rb
@@ -1,7 +1,7 @@
 # encoding: utf-8
 Rails.application.config.to_prepare do
 	if ActiveRecord::Base.connection.table_exists?("goldencobra_articles") && ActiveRecord::Base.connection.table_exists?("goldencobra_articletypes")
-		Goldencobra::Article.scoped.pluck(:article_type).uniq.each do |at|
+		Goldencobra::Article.article_types_for_select.each do |at|
 			if Goldencobra::Articletype.find_by_name(at).blank?
 				Goldencobra::Articletype.create(:name => at, :default_template_file => "application")
 				puts "Default Articletype created for #{at}"
