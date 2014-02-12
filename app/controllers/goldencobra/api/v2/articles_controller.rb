3bb6eda9076fd2936b97563c0decff62a6e49867
Andreas Marc Klingler <post@andreas-klingler.de>
2014-02-12 10:38:33 +0100

    gc-51 Struktur für API-Version 2 mit articles#search angelegt.

---
+++ app/controllers/goldencobra/api/v2/articles_controller.rb
@@ -0,0 +1,14 @@
+module Goldencobra
+  module Api
+    module V2
+      class ArticlesController < ActionController::Base
+
+        respond_to :json
+
+        def search
+        end
+
+      end
+    end
+  end
+end
