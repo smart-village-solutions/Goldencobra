76b6d1c3312df8fdb9634c4a84915ab1edb329b7
Andreas Marc Klingler <post@andreas-klingler.de>
2014-04-11 17:37:03 +0200

    gc-128 Create action added to APIv2:ArticlesController - probably not final yet.

--- app/controllers/goldencobra/api/v2/articles_controller.rb
+++ app/controllers/goldencobra/api/v2/articles_controller.rb
@@ -5,6 +5,7 @@ module Goldencobra
     module V2
       class ArticlesController < ActionController::Base
 
+        include Goldencobra::Api::V2::ArticlesHelper
         respond_to :json
 
         # /api/v2/articles/search[.json]
@@ -22,8 +23,33 @@ module Goldencobra
             render status: 200, json: {}
           else
             # 1. Do the search and return the search result array.
-            render status: 200, json: Goldencobra::Article.simple_search( ActionController::Base.helpers.sanitize(params[:q]) ).to_json
+            render status: 200, json: Goldencobra::Article.simple_search(ActionController::Base.helpers.sanitize(params[:q])).to_json
           end
+        end
+
+
+        # /api/v2/articles/create[.json]
+        # ---------------------------------------------------------------------------------------
+        def create
+          # Check if a user is currently logged in.
+          unless current_user
+            render status: 403, json: {}
+            return
+          end
+
+          # Check if we do have an article passed by the parameters.
+          unless params[:article]
+            render status: 400, json: {}
+            return
+          end
+
+          # Try to save the article
+          if create_article(params[:article])
+            render status: 200, json: {}
+          else
+            render status: 500, json: {}
+          end
+
         end
 
       end
