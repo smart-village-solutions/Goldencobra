1ec604d3b394e3719c24ee1c3db6711d85d842ab
Andreas Marc Klingler <post@andreas-klingler.de>
2014-05-12 09:40:32 +0200

    GC-128 Logik von Helper in Controller verschoben, kleinere Verbesserungen

--- app/controllers/goldencobra/api/v2/articles_controller.rb
+++ app/controllers/goldencobra/api/v2/articles_controller.rb
@@ -5,7 +5,6 @@ module Goldencobra
     module V2
       class ArticlesController < ActionController::Base
 
-        include Goldencobra::Api::V2::ArticlesHelper
         respond_to :json
 
         # /api/v2/articles/search[.json]
@@ -14,16 +13,18 @@ module Goldencobra
 
           # Check if we have an argument.
           unless params[:q]
-            render status: 200, json: {}
+            render status: 200, json: { :status => 200 }
             return
           end
 
           # Check if the query string contains something.
           if params[:q].length == 0
-            render status: 200, json: {}
+            render status: 200, json: { :status => 200 }
           else
-            # 1. Do the search and return the search result array.
-            render status: 200, json: Goldencobra::Article.simple_search(ActionController::Base.helpers.sanitize(params[:q])).to_json
+            # Search and return the result array.
+            render status: 200, json: Goldencobra::Article.simple_search(
+                ActionController::Base.helpers.sanitize(params[:q])
+            ).to_json
           end
         end
 
@@ -33,24 +34,57 @@ module Goldencobra
         def create
           # Check if a user is currently logged in.
           unless current_user
-            render status: 403, json: {}
+            render status: 403, json: { :status => 403 }
             return
           end
 
           # Check if we do have an article passed by the parameters.
           unless params[:article]
-            render status: 400, json: {}
+            render status: 400, json: { :status => 400 }
             return
           end
 
           # Try to save the article
           if create_article(params[:article])
-            render status: 200, json: {}
+            render status: 200, json: { :status => 200 }
           else
-            render status: 500, json: {}
+            render status: 500, json: { :status => 500 }
           end
 
         end
+
+
+        protected
+
+        # Creates an article from the given article array.
+        # ---------------------------------------------------------------------------------------
+        def create_article(article_param)
+
+          # Input validation
+          return nil unless article_param
+          return nil unless params[:article]
+
+          # Create a new article
+          new_article = Goldencobra::Article.new(
+              :title => params[:article][:title],
+              :breadcrumb => params[:article][:breadcrumb],
+              :author_id => current_user.id
+          )
+
+          if params[:article][:article_type]
+            new_article.article_type = params[:article][:article_type]
+          else
+            new_article.article_type = 'Default Show'
+          end
+
+          # If there is a parent article, we need to do the same things again.
+          new_article.ancestry = create_article(params[:article][:ancestry]) if params[:article][:ancestry]
+
+          # Try to save the article
+          new_article.save ? new_article : nil
+
+        end
+
 
       end
     end
