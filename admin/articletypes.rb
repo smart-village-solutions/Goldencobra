4a5ac49cca95a7a30f6abc7c538f862aa9676506
Daniel Molnar <dm@ikusei.de>
2014-05-21 17:42:43 +0200

    fixed differences in yml files | MET-114

--- admin/articletypes.rb
+++ admin/articletypes.rb
@@ -13,17 +13,17 @@ ActiveAdmin.register Goldencobra::Articletype, :as => "Articletype" do
   end
 
   form :html => { :enctype => "multipart/form-data" }  do |f|
-    f.inputs "Allgemein" do
+    f.inputs I18n.t('active_admin.articletypes.general') do
       f.input :default_template_file, :as => :select, :collection => Goldencobra::Article.templates_for_select, :include_blank => false
     end
-    f.inputs "Artikel Felder", :class => "foldable closed inputs" do
+    f.inputs I18n.t('active_admin.articletypes.article_fields'), :class => "foldable closed inputs" do
       f.has_many :fieldgroups do |fg|
         fg.input :title
-        fg.input :position, :as => :select, :collection => [["First Block","first_block"],["Last Block","last_block"]], :include_blank => false, :hint => "Zwischen dem ersten und dem letzten Block kommen die speziellen Feldoptionen eines Articeltypen"
-        fg.input :foldable, :hint => "Kann man den Bereich auf und zu klappen?"
-        fg.input :closed, :hint => "Ist der Bereich beim laden geöffnet oder geschlossen"
-        fg.input :expert, :hint => "Ist der Bereich nur im Expertenmodus sichtbar"
-        fg.input :sorter, :hint => "Sortiernummer/Position"
+        fg.input :position, :as => :select, :collection => [["First Block","first_block"], ["Last Block","last_block"]], :include_blank => false, :hint => I18n.t('active_admin.articletypes.position_hint')
+        fg.input :foldable, :hint => I18n.t('active_admin.articletypes.foldable_hint')
+        fg.input :closed, :hint => I18n.t('active_admin.articletypes.closed_hint')
+        fg.input :expert, :hint => I18n.t('active_admin.articletypes.expert_hint')
+        fg.input :sorter, :hint => I18n.t('active_admin.articletypes.sorter_hint')
         fg.input :_destroy, :as => :boolean
         fg.has_many :fields do |fo|
           fo.input :fieldname, :as => :select, :collection => Goldencobra::Articletype::ArticleFieldOptions.keys, :include_blank => false
