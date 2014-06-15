efdb17450fd98bdb561798259e4ac7d642d6087f
Marco Metz <marco.metz@gmail.com>
2014-06-15 21:14:51 +0200

    link checker too lomg value

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -1,3 +1,5 @@
+# encoding: utf-8
+
 # Use this hook to configure devise mailer, warden hooks and so forth.
 # Many of these configuration options can be set straight in your model.
 require 'openid/store/filesystem'
@@ -134,7 +136,7 @@ Devise.setup do |config|
 
   # Number of authentication tries before locking an account if lock_strategy
   # is failed attempts.
-  config.maximum_attempts = 20
+  config.maximum_attempts = 8
 
   # Time interval to unlock the account if :time is enabled as unlock_strategy.
   config.unlock_in = 1.hour
