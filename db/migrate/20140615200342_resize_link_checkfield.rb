72d724fedb2d4a5e91e6fbccb8497b92e7ab5219
Marco Metz <marco.metz@gmail.com>
2014-06-15 22:17:33 +0200

    resize link_checker

--- db/migrate/20140615200342_resize_link_checkfield.rb
+++ db/migrate/20140615200342_resize_link_checkfield.rb
@@ -3,9 +3,11 @@
 class ResizeLinkCheckfield < ActiveRecord::Migration
   def up
     change_column :goldencobra_articles, :link_checker, :text, :limit => 4294967295
+    change_column :versions, :object, :text, :limit => 4294967295
   end
 
   def down
     change_column :goldencobra_articles, :link_checker, :text
+    change_column :versions, :object, :text
   end
 end
