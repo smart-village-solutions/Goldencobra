fa7a1aba6b9a0a5e6a3625f741de8b64a1d9a357
Daniel Molnar <dm@ikusei.de>
2014-06-18 13:38:42 +0200

    author save fix | GC-143

--- app/models/goldencobra/author.rb
+++ app/models/goldencobra/author.rb
@@ -3,7 +3,8 @@
 module Goldencobra
 	class Author < ActiveRecord::Base
 		has_many :article
-		web_url :goolgeplus
+    
+		web_url :googleplus
 
 		def title
 			[self.try(:firstname),self.try(:lastname)].join(" ")
