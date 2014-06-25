9579725bb5f439ad95bc64f46a88196a5a5b5566
Marco Metz <marco.metz@gmail.com>
2014-06-25 20:02:12 +0200

    creator id

--- app/models/goldencobra/article.rb
+++ app/models/goldencobra/article.rb
@@ -75,7 +75,7 @@ module Goldencobra
     belongs_to :articletype, :class_name => Goldencobra::Articletype, :foreign_key => "article_type", :primary_key => "name"
     belongs_to :creator, :class_name => User, :foreign_key => "creator_id"
 
-    belongs_to :author
+    belongs_to :author, :class_name => Goldencobra::Author
 
     accepts_nested_attributes_for :metatags, :allow_destroy => true, :reject_if => proc { |attributes| attributes['value'].blank? }
     accepts_nested_attributes_for :article_images, :allow_destroy => true
