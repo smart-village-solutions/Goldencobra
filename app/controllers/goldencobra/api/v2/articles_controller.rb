ea9ffe5487d9fb5ad39e7285e43747b0aee8adac
Marco Metz <marco.metz@gmail.com>
2014-07-02 12:13:15 +0200

    update api

--- app/controllers/goldencobra/api/v2/articles_controller.rb
+++ app/controllers/goldencobra/api/v2/articles_controller.rb
@@ -69,6 +69,41 @@ module Goldencobra
         end
 
 
+        def update
+          unless current_user
+            render status: 403, json: { :status => 403 }
+            return
+          end
+
+          # Check if we do have an article passed by the parameters.
+          unless params[:article]
+            render status: 400, json: { :status => 400, :error => "article data missing" }
+            return
+          end
+
+          #check if an external referee is passed by the parameters
+          unless params[:referee_id]
+            render status: 400, json: { :status => 400, :error => "referee_id missing"  }
+            return
+          end
+
+          #check if Article already exists by comparing external referee and current user of caller
+          existing_articles = Goldencobra::Article.where(:creator_id => current_user.id, :external_referee_id => params[:referee_id])
+          if existing_articles.blank?
+            render status: 423, json: { :status => 423, :error => "article not found", :id => nil  }
+            return
+          end
+
+          # Try to save the article
+          response = update_article(params[:article])
+          if response.id.present?
+            render status: 200, json: { :status => 200, :id => response.id }
+          else
+            render status: 500, json: { :status => 500, :error => response.errors, :id => nil }
+          end
+        end
+
+
         protected
 
         # Creates an article from the given article array.
@@ -103,6 +138,31 @@ module Goldencobra
           # Try to save the article
           new_article.save
           return new_article
+        end
+
+
+        def update_article(article_param)
+          # Input validation
+          return nil unless article_param
+          return nil unless params[:article]
+          return nil unless current_user
+          return nil unless params[:referee_id]
+
+          # Get existing article
+          article = Goldencobra::Article.where(:creator_id => current_user.id).find_by_external_referee_id(params[:referee_id])
+
+          if params[:author].present? && params[:author][:lastname].present?
+            author = Goldencobra::Author.find_or_create_by_lastname(params[:author][:lastname])
+            article.author = author
+            article.save
+          end
+
+          # Update existing article
+          article.update_attributes(params[:article])
+
+          # Try to save the article
+          return article
+
         end
 
 
