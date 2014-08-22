f89f5ea986fee6f13a5dad814393b02920ef01b3
Marco Metz <marco.metz@gmail.com>
2014-08-22 14:44:41 +0200

    encoding utf added

--- test/dummy/app/controllers/extend_goldencobra_articles_controller.rb
+++ test/dummy/app/controllers/extend_goldencobra_articles_controller.rb
@@ -1,3 +1,5 @@
+# encoding: utf-8
+
 module ExtendGoldencobraArticlesController
 
   def before_init
--- test/dummy/config/routes.rb
+++ test/dummy/config/routes.rb
@@ -1,3 +1,5 @@
+# encoding: utf-8
+
 Rails.application.routes.draw do
   ActiveAdmin.routes(self)
   devise_for :users, ActiveAdmin::Devise.config
