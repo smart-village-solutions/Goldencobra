38ca385a8133698f9870db19fcf2dd98b8bf9878
Marco Metz <marco.metz@gmail.com>
2014-09-10 10:54:30 +0200

    comment patch

--- admin/comments.rb
+++ admin/comments.rb
@@ -10,7 +10,7 @@ ActiveAdmin.register Goldencobra::Comment, :as => "article_comment" do
       f.input :active
       f.input :approved
       f.input :reported
-      f.input :parent, :as => :select, :collection => Goldencobra::Comment.scoped
+      f.input :parent_id, :as => :select, :collection => Goldencobra::Comment.scoped
       f.input :article, :as => :select, :collection => Goldencobra::Article.scoped
       f.input :commentator, :as => :select, :collection => Goldencobra::Setting.for_key("goldencobra.comments.commentator").constantize.scoped
     end
