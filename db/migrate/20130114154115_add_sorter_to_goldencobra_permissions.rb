6b07b2b68a489366e921c3df04f4164d9af2135d
Marco Metz <marco.metz@gmail.com>
2015-02-06 15:54:19 +0100

    installer routine und migration angepasst

--- db/migrate/20130114154115_add_sorter_to_goldencobra_permissions.rb
+++ db/migrate/20130114154115_add_sorter_to_goldencobra_permissions.rb
@@ -2,6 +2,7 @@
 
 class AddSorterToGoldencobraPermissions < ActiveRecord::Migration
   def change
-    add_column :goldencobra_permissions, :sorter_id, :integer, :default => 0
+    # das hier ist schon in der Migration CreateGoldencobraPermissions enthalten
+    # add_column :goldencobra_permissions, :sorter_id, :integer, :default => 0
   end
 end
