10e8e14769bfd21657e7fc9ee9c578b55df53c87
Holger Frohloff <holger@5minutenpause.com>
2015-05-20 14:07:52 +0200

    Adds SimpleCov for code coverage metrics

--- test/dummy/spec/spec_helper.rb
+++ test/dummy/spec/spec_helper.rb
@@ -1,5 +1,17 @@
 # encoding: utf-8
 
+require 'simplecov'
+SimpleCov.root(File.expand_path("../../", __FILE__))
+SimpleCov.start :rails do
+  filters.clear # This will remove the :root_filter and :bundler_filter that come via simplecov's defaults
+  add_filter do |src|
+    !(src.filename =~ /^#{SimpleCov.root}/) unless (src.filename =~ /app/ || src.filename =~ /config\/initializers/)
+  end
+  add_filter ".bundle"
+  add_filter ".rbenv"
+  add_filter "/spec/"
+end
+
 # This file is copied to spec/ when you run 'rails generate rspec:install'
 ENV["RAILS_ENV"] ||= 'test'
 require File.expand_path("../../config/environment", __FILE__)
