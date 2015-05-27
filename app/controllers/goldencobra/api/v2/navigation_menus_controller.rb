5c01cb824d82ecead721b8d78219ef8fe5b79c02
Holger Frohloff <holger@5minutenpause.com>
2015-05-27 22:01:54 +0200

    Set active classes during rendering of menu
    Introduces a callback after updating the list component. Set depth of children elements.

--- app/controllers/goldencobra/api/v2/navigation_menus_controller.rb
+++ app/controllers/goldencobra/api/v2/navigation_menus_controller.rb
@@ -2,28 +2,28 @@ module Goldencobra
   module Api
     module V2
       class NavigationMenusController < ActionController::Base
-        
+
         before_filter :get_master_element
 
         respond_to :json
 
 
-        # Get active IDs of current request/url 
+        # Get active IDs of current request/url
         # Searches in Subtree of a given MasterElement
         # @param id [String] [ID of Submenu to search for]
         # @param url [String] Url to Search vor Active MenueIDs
-        # 
-        # Beispiel
-        # http://localhost:3000/api/v2/navigation_menus/active?id=1&url="/seite1/seite-a"
-        # 
+        #
+        # Example
+        # http://localhost:3000/api/v2/navigation_menus/active?id=1&url=/seite1/seite-a
+        #
         # @return [Integer] Array if Integers als Menue IDs
         def active_ids
           # @master_element is set by before filter
-          
+
           if params[:url].present?
             url_to_search = params[:url]
 
-            #URI parse domain and url path
+            # URI parse domain and url path
             parsed_url = URI(url_to_search)
 
             current_menue = @master_element.subtree.active.where(:target => parsed_url.path).first
@@ -43,10 +43,10 @@ module Goldencobra
 
 
 
-        # /api/v2/uploads[.json]
-        # 
+        # /api/v2/navigation_menus[.json]
+        #
         # @param id [String] [ID of Submenu to render]
-        # @param methods [String] "description,image"
+        # @param methods [String] "css_class,liquid_description,navigation_image,description_title,call_to_action_name"
         # [
         #     {
         #         "id": 2,
@@ -81,10 +81,10 @@ module Goldencobra
         #         "children": []
         #     }
         # ]
-        # 
-        # 
-        # 
-        #  Response Menü als JSON
+        #
+        #
+        #
+        #  Response as JSON
         #  {
         #     "active": true,
         #     "ancestry": null,
@@ -103,42 +103,42 @@ module Goldencobra
         #     "updated_at": "2014-10-10T09:43:54+02:00"
         #  }
         #
-        # @return [json] Liefert alle Menüs :id, :complete_list_name
+        # @return [json] All menus with :id, :complete_list_name, etc.
         def index
           # @master_element is set by before filter
-          
+
           # Generate cache key: if any Menuitem is changed => invalidate
           last_modified = Goldencobra::Menue.order(:updated_at).pluck(:updated_at).last.to_i
           cache_sub_key = [ params[:depth], params[:offset], display_methods, last_modified ].flatten.join("_")
           cache_key = "Navigation/menue_#{@master_element.id}/#{Digest::MD5.hexdigest(cache_sub_key)}"
-        
-          #Gibt es Das Menü bereits im Cache?
-          if Rails.cache.exist?(cache_key)
-            @json_tree = Rails.cache.read(cache_key)
-          else
+
+          #Gibt es das Menü bereits im Cache?
+          # if Rails.cache.exist?(cache_key)
+            # @json_tree = Rails.cache.read(cache_key)
+          # else
             # How many levels of subtree should be displayed
             depth = params[:depth].present? ? params[:depth].to_i : 9999
 
             # How many levels of subtree should be skipped
             offset = params[:offset].present? ? params[:offset].to_i : 0
 
-            #Current Level of master element
+            # Current Level of master element
             current_depth = @master_element.ancestry_depth
 
-            # Get all Menus of Subtree from startlevel to endlevel
+            # Get all menus of subtree from startlevel to endlevel
             menus = @master_element.subtree.active.visible
             menus = filter_elements(menus)
             menus = menus.after_depth(current_depth + offset).to_depth(current_depth + depth)
 
-            #Prepare Menue Data to Display
+            # Prepare menu data to display
             menue_data_as_json = menus.arrange(:order => :sorter)
 
-            #JsonTree der Dargestellt werden soll, übergabe der display methoden
+            # JsonTree to display, use specified display_methods
             @json_tree = Goldencobra::Menue.json_tree(menue_data_as_json, display_methods )
 
-            #Schreibe das Ergebnis zusätzlich in den Cache
-            Rails.cache.write(cache_key, @json_tree)
-          end
+            # Save result to cache
+            # Rails.cache.write(cache_key, @json_tree)
+          # end
 
           respond_to do |format|
             format.json { render json: @json_tree }
@@ -147,10 +147,10 @@ module Goldencobra
 
 
 
-        private 
+        private
 
         def get_master_element
-          #find MasterElement by ID, Name or TargetUrl
+          # find MasterElement by ID, Name or TargetUrl
           # Priority ID > Name > Target
           if params[:id]
             @master_element = Goldencobra::Menue.active.find_by_id(params[:id])
@@ -162,7 +162,7 @@ module Goldencobra
             raise "No Menueitem to search for"
           end
 
-          #Validate presents of master element
+          # Validate presence of master element
           raise "No Menueitem found" if @master_element.blank?
         end
 
