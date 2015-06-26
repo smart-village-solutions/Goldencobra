8e92fbe4e1c7ed74efe097f66bdb43f605de9e13
Holger Frohloff <holger@5minutenpause.com>
2015-06-26 18:16:47 +0200

    Resolves requirements for different gems
    Rails 4 needs i18n-active_record from the Github repo. The published version
    seems to have errors. It is as of yet unsure how to proceed.
    GC-169

--- lib/goldencobra.rb
+++ lib/goldencobra.rb
@@ -1,3 +1,16 @@
+Gem.loaded_specs['goldencobra'].dependencies.each do |d|
+  next if d.name == "rubyzip"
+  if d.name == "oa-oauth"
+    require "omniauth/oauth"
+  elsif d.name == "oa-openid"
+    require "omniauth/openid"
+  elsif d.name == "i18n-active_record"
+    require "i18n/active_record"
+  else
+    require d.name
+  end
+end
+
 require "goldencobra/engine"
 module Goldencobra
 end
