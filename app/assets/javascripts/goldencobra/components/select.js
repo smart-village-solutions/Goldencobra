4e3de80704332fb793af4c95fdd13bc14d15292f
Daniel Molnar <dm@ikusei.de>
2015-07-14 13:25:09 +0200

    load overview trees fixed after integration of active_model_serializers

--- app/assets/javascripts/goldencobra/components/select.js
+++ app/assets/javascripts/goldencobra/components/select.js
@@ -1,32 +1,37 @@
-var OptionItem = React.createClass({displayName: "OptionItem",
+var OptionItem = React.createClass({
+  displayName: 'OptionItem',
   render: function () {
     return (
-      React.createElement("option", {value: this.props.value}, this.props.label)
+      React.createElement('option', { value: this.props.value }, this.props.label)
     );
   }
 });
 
-var SelectList = React.createClass({displayName: "SelectList",
+var SelectList = React.createClass({
+  displayName: 'SelectList',
   render: function () {
-    var optionNodes = this.props.options.map(function (el) {
+    var optionNodes = this.props.options.articles.map(function (el) {
       return (
-        React.createElement(OptionItem, {value: el.value, label: el.label, key: el.value})
+        React.createElement(OptionItem, { value: el.value, label: el.label, key: el.value })
       );
     });
 
     var emptyOption;
 
     if (this.props.firstBlank == true) {
-      emptyOption = React.createElement("option", {value: ""}, this.props.label)
+      emptyOption = React.createElement('option', { value: '' }, this.props.label);
     }
 
     return (
-      React.createElement("select", {className: this.props.className + " select-list reacted", id: this.props.id, style: {width:"70%"}, value: this.props.value, name: this.props.name},
+      React.createElement('select', {
+          className: this.props.className + ' select-list reacted',
+          id: this.props.id, style: { width: '70%' },
+          value: this.props.value,
+          name: this.props.name
+        },
         emptyOption,
         optionNodes
       )
     );
   }
 });
-
-
