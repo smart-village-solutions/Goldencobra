596405bf905fb9faab5000b11561022aa319d1f5
Daniel Molnar <dm@ikusei.de>
2015-08-06 15:56:57 +0200

    reset article types to default after destroy | GC-169

--- app/models/goldencobra/articletype.rb
+++ app/models/goldencobra/articletype.rb
@@ -3,11 +3,15 @@
 module Goldencobra
   class Articletype < ActiveRecord::Base
     attr_accessible :default_template_file, :name, :fieldgroups_attributes
+
     has_many :articles, class_name: Goldencobra::Article, foreign_key: :article_type, primary_key: :name
+    has_many :fieldgroups, class_name: Goldencobra::ArticletypeGroup, dependent: :destroy
+
+    accepts_nested_attributes_for :fieldgroups, allow_destroy: true
+
     validates_uniqueness_of :name
 
-    has_many :fieldgroups, class_name: Goldencobra::ArticletypeGroup, dependent: :destroy
-    accepts_nested_attributes_for :fieldgroups, allow_destroy: true
+    after_destroy :set_defaults
 
     ArticleFieldOptions = {
       global_sorting_id: %{<% f.input :global_sorting_id, hint: I18n.t("goldencobra.article_field_hints.global_sorting_id") %>},
@@ -51,5 +55,58 @@ module Goldencobra
         ai.input :_destroy, as: :boolean
       end %>}
     }
+
+    def set_defaults
+      Goldencobra::Articletype.reset_to_default
+    end
+
+    def self.reset_to_default
+      if ActiveRecord::Base.connection.table_exists?("goldencobra_articles") && ActiveRecord::Base.connection.table_exists?("goldencobra_articletypes")
+        Goldencobra::Article.article_types_for_select.each do |at|
+          if Goldencobra::Articletype.find_by_name(at).blank?
+            Goldencobra::Articletype.create(name: at, default_template_file: "application")
+            puts "Default Articletype created for #{at}"
+          end
+        end
+
+        if ActiveRecord::Base.connection.table_exists?("goldencobra_articletype_groups")
+          Goldencobra::Articletype.all.each do |at|
+            # install basic set of fieldgroups and fields if no one is set up
+            if !at.try(:fieldgroups).any?
+              a1 = at.fieldgroups.create(title: "Allgemein", position: "first_block", foldable: true, closed: false, expert: false, sorter: 1)
+              a1.fields.create(fieldname: "active", sorter: 1)
+              a1.fields.create(fieldname: "breadcrumb", sorter: 5)
+              a1.fields.create(fieldname: "subtitle", sorter: 10)
+              a1.fields.create(fieldname: "title", sorter: 20)
+              a1.fields.create(fieldname: "teaser", sorter: 30)
+              a1.fields.create(fieldname: "content", sorter: 40)
+              a1.fields.create(fieldname: "tag_list", sorter: 50)
+
+              a2 = at.fieldgroups.create(title: "Medien", position: "last_block", foldable: true, closed: true, expert: false, sorter: 1)
+              a2.fields.create(fieldname: "article_images", sorter: 1)
+
+              a3 = at.fieldgroups.create(title: "Metadescriptions", position: "last_block", foldable: true, closed: true, expert: true, sorter: 3)
+              a3.fields.create(fieldname: "metatags", sorter: 1)
+
+              a4 = at.fieldgroups.create(title: "Einstellungen", position: "last_block", foldable: true, closed: true, expert: true, sorter: 4)
+              a4.fields.create(fieldname: "frontend_tag_list", sorter: 10)
+              a4.fields.create(fieldname: "url_name", sorter: 20)
+              a4.fields.create(fieldname: "parent_id", sorter: 30)
+              a4.fields.create(fieldname: "canonical_url", sorter: 40)
+              a4.fields.create(fieldname: "active_since", sorter: 50)
+              a4.fields.create(fieldname: "robots_no_index", sorter: 60)
+              a4.fields.create(fieldname: "cacheable", sorter: 70)
+              a4.fields.create(fieldname: "dynamic_redirection", sorter: 100)
+              a4.fields.create(fieldname: "external_url_redirect", sorter: 110)
+              a4.fields.create(fieldname: "redirect_link_title", sorter: 120)
+              a4.fields.create(fieldname: "redirection_target_in_new_window", sorter: 130)
+              a4.fields.create(fieldname: "author", sorter: 140)
+
+              puts "Default Fieldoptions recreated for #{at.try(:name)}"
+            end
+          end
+        end
+      end
+    end
   end
 end
