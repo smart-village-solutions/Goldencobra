7f434643469f08c63a3df58c39d86890b794e2a1
Holger Frohloff <holger@5minutenpause.com>
2015-08-17 17:39:10 +0200

    Fixes rspec syntax deprecation warning
    Why:
    
    * When running tests it distracts from the actual output if there are many
    warnings. Also you don't know when the deprecations will be removed.
    
    This change addresses the need by:
    
    * Update to Rails 4.2 and fix everything.

--- test/dummy/spec/models/location_spec.rb
+++ test/dummy/spec/models/location_spec.rb
@@ -5,23 +5,25 @@ require 'spec_helper'
 describe Goldencobra::Location do
 
   it "should have a latitude and longitude" do
-    location = Goldencobra::Location.create(:street => "Zossener Str. 55", :city => "Berlin", :zip => "10961")
-    location.lat.should_not == nil
-    location.lng.should_not == nil
-    Goldencobra::Location.find_by_zip("10961").street.should == "Zossener Str. 55"
+    location = Goldencobra::Location.create(street: "Zossener Str. 55", 
+                                              city: "Berlin",
+                                               zip: "10961")
+    expect(location.lat).to_not eq(nil)
+    expect(location.lng).to_not eq(nil)
+    expect(Goldencobra::Location.find_by_zip("10961").street).to eq "Zossener Str. 55"
   end
 
   it "should have no latitude and longitude because of missing city" do
     location = Goldencobra::Location.create(:street => "Puccinistrasse 40", :city => "", :zip => "13088")
-    location.lat.should == nil
-    location.lng.should == nil
-    Goldencobra::Location.find_by_zip("13088").street.should == "Puccinistrasse 40"
+    expect(location.lat).to eq(nil)
+    expect(location.lng).to eq(nil)
+    expect(Goldencobra::Location.find_by_zip("13088").street).to eq "Puccinistrasse 40"
   end
 
   it "should not have a latitude and longitude" do
     location = Goldencobra::Location.create(:street => " ", :city => "", :zip => "")
-    location.lat.should == nil
-    location.lng.should == nil
+    expect(location.lat).to eq(nil)
+    expect(location.lng).to eq(nil)
   end
 
 
