4b35da6a8fcf280da1edc589aaaeb739d53649ff
Holger Frohloff <holger@5minutenpause.com>
2015-08-17 18:29:35 +0200

    Raises test coverage to 63.06%
    Adds some missing tests.

--- test/dummy/spec/models/location_spec.rb
+++ test/dummy/spec/models/location_spec.rb
@@ -5,7 +5,7 @@ require 'spec_helper'
 describe Goldencobra::Location do
 
   before do
-    Geocoder.configure(:lookup => :test)
+    Geocoder.configure(lookup: :test)
 
     Geocoder::Lookup::Test.add_stub(
       "Zossener Str. 55, 10961 Berlin", [
@@ -21,27 +21,40 @@ describe Goldencobra::Location do
                     ]
     )
   end
-  it "should have a latitude and longitude" do
-    location = Goldencobra::Location.create(street: "Zossener Str. 55", 
-                                              city: "Berlin",
-                                               zip: "10961")
-    expect(location.lat).to_not eq(nil)
-    expect(location.lng).to_not eq(nil)
-    expect(Goldencobra::Location.find_by_zip("10961").street).to eq "Zossener Str. 55"
+
+  context "with a valid address" do
+    it "should have a latitude and longitude" do
+      location = Goldencobra::Location.create(street: "Zossener Str. 55",
+                                                city: "Berlin",
+                                                 zip: "10961")
+      expect(location.lat).to_not eq(nil)
+      expect(location.lng).to_not eq(nil)
+      expect(Goldencobra::Location.find_by_zip("10961").street).to eq "Zossener Str. 55"
+    end
+
+    describe "#title" do
+      it "returns the complete location" do
+        location = Goldencobra::Location.create(street: "Zossener Str. 55",
+                                                city: "Berlin",
+                                                zip: "10961")
+
+        expect(location.title).to eq("Zossener Str. 55, 10961 Berlin")
+      end
+    end
   end
 
-  it "should have no latitude and longitude because of missing city" do
-    location = Goldencobra::Location.create(street: "Puccinistrasse 40", city: "", zip: "13088")
-    expect(location.lat).to eq(nil)
-    expect(location.lng).to eq(nil)
-    expect(Goldencobra::Location.find_by_zip("13088").street).to eq "Puccinistrasse 40"
+  context "with an invalid address" do
+    it "should have no latitude and longitude because of missing city" do
+      location = Goldencobra::Location.create(street: "Puccinistrasse 40", city: "", zip: "13088")
+      expect(location.lat).to eq(nil)
+      expect(location.lng).to eq(nil)
+      expect(Goldencobra::Location.find_by_zip("13088").street).to eq "Puccinistrasse 40"
+    end
+
+    it "should not have a latitude and longitude" do
+      location = Goldencobra::Location.create(street: " ", city: "", zip: "")
+      expect(location.lat).to eq(nil)
+      expect(location.lng).to eq(nil)
+    end
   end
-
-  it "should not have a latitude and longitude" do
-    location = Goldencobra::Location.create(street: " ", city: "", zip: "")
-    expect(location.lat).to eq(nil)
-    expect(location.lng).to eq(nil)
-  end
-
-
 end
