1f38cd0c26fa7cf091bf81b2339dca7613165192
Marco Metz <metz@ikusei.de>
2015-08-19 10:23:28 +0200

    Fix Für Filter in Roles

--- admin/roles.rb
+++ admin/roles.rb
@@ -3,6 +3,8 @@
 ActiveAdmin.register Goldencobra::Role, :as => "Role" do
   menu :parent => I18n.t("settings", :scope => ["active_admin","menue"]), :label => I18n.t('active_admin.roles.as'), :if => proc{can?(:update, Goldencobra::Role)}
 
+  filter :name
+
   action_item :prev_item, only: [:edit, :show] do
     render partial: '/goldencobra/admin/shared/prev_item'
   end
