17bfaa93265fb43ad63860788300fd8c46147d82
Daniel Molnar <dm@ikusei.de>
2015-08-24 17:29:55 +0200

    default value in index options for descendant articles set to 1

--- db/migrate/20150819121715_add_descendentsdepth_to_goldencobra_articles.rb
+++ db/migrate/20150819121715_add_descendentsdepth_to_goldencobra_articles.rb
@@ -1,6 +1,6 @@
 class AddDescendentsdepthToGoldencobraArticles < ActiveRecord::Migration
   def change
-    add_column :goldencobra_articles, :index_of_articles_descendents_depth, :string, :default => "all"
-    add_column :goldencobra_articles, :ancestry_depth, :integer, :default => 0
+    add_column :goldencobra_articles, :index_of_articles_descendents_depth, :string, default: "1"
+    add_column :goldencobra_articles, :ancestry_depth, :integer, default: 0
   end
 end
