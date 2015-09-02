139ce16b7f57d68a506d18f85064a2dc80b9e709
Holger Frohloff <holger@5minutenpause.com>
2015-09-02 19:57:04 +0200

    Simplifies the code after PR feedback
    JIRA-TASK: GCZ-32

--- app/controllers/goldencobra/api/v2/navigation_menus_controller.rb
+++ app/controllers/goldencobra/api/v2/navigation_menus_controller.rb
@@ -192,20 +192,20 @@ module Goldencobra
         end
 
         def find_menu_with_matching_path(parsed_url)
-          parsed_url = Addressable::URI.parse(parsed_url.path.chomp("/"))
+          parsed_url = parsed_url.path.chomp("/")
 
           # All Menu records that match the path but might differ by parameters
           possible_menues = @master_element.subtree.active
-            .where("goldencobra_menues.target LIKE '#{parsed_url.path}%' ")
+            .where("goldencobra_menues.target LIKE '#{parsed_url}%' ")
 
           # Select matching record (without matching for parameters)
           current_menue = possible_menues.select do |pos_menu|
-            Addressable::URI.parse(pos_menu.target).path == parsed_url.path
+            Addressable::URI.parse(pos_menu.target).path == parsed_url
           end.first
 
           if current_menue.blank?
             # Try for a path without last element
-            reduced_url = parsed_url.path.split("/").tap(&:pop).join("/")
+            reduced_url = parsed_url.split("/").tap(&:pop).join("/")
             return find_menu_with_matching_path(Addressable::URI.parse(reduced_url))
           end
 
