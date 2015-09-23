f52bd10718959d5e0965ce28d297fb566aaa5251
Holger Frohloff <holger@5minutenpause.com>
2015-09-23 20:33:56 +0200

    Refactors Api::V2::ArticlesController
    Deletes unnecessary methods and cleans up the process flow.
    
    JIRA-TASK: GCZ-34

--- test/dummy/spec/controllers/api/v2/articles_controller_spec.rb
+++ test/dummy/spec/controllers/api/v2/articles_controller_spec.rb
@@ -1,38 +1,48 @@
-require 'spec_helper'
+require "spec_helper"
 
 describe Goldencobra::Api::V2::ArticlesController, type: :controller do
   before(:each) { @routes = Goldencobra::Engine.routes }
 
-  describe 'POST #create' do
-
-    describe 'without an user who is logged in' do
-
+  describe "POST #create" do
+    context "when not logged in" do
       before do
-        post :create,
-             format: :json
+        post :create, format: :json
       end
 
       it "should return a json status forbidden" do
         expect(response.response_code).to eq 403
       end
-
     end
 
+    context "as a user who is logged in" do
+      before do
+        @request.env["devise.mapping"] = Devise.mappings[:user]
+        @user = FactoryGirl.create(:user)
+        sign_in @user
+
+        post :create, format: :json
+      end
+
+      it "should return a json status 400" do
+        expect(response.response_code).to eq 400
+      end
+    end
   end
 
-  describe 'with an user who is logged in' do
 
-    before do
-      @request.env["devise.mapping"] = Devise.mappings[:user]
-      @user = FactoryGirl.create(:user)
-      sign_in @user
+  describe "#update" do
+    context "without an existing article" do
+      before do
+        @request.env["devise.mapping"] = Devise.mappings[:user]
+        @user = FactoryGirl.create(:user)
+        sign_in @user
+      end
 
-      post :create,
-           format: :json
-    end
+      it "returns an error 423" do
+        post :update
 
-    it "should return a json status 400, 'article data missing'" do
-      expect(response.response_code).to eq 400
+        expect(response.response_code).to eq(423)
+      end
     end
   end
 end
