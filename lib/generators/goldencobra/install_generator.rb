720fd5ee11bda79d862e865a927fc8d22456d4b0
Daniel Molnar <dm@ikusei.de>
2015-10-20 12:13:13 +0200

    Template files added and install generator updated
    Bootstrap integrated as default and new templates for golden cobra install
    
    JIRA-TASK: KSSA-5

--- lib/generators/goldencobra/install_generator.rb
+++ lib/generators/goldencobra/install_generator.rb
@@ -1,6 +1,4 @@
-# encoding: utf-8
-
-require 'securerandom'
+require "securerandom"
 
 module Goldencobra
   module Generators
@@ -14,41 +12,41 @@ module Goldencobra
         if yes?("Would you like to configure a .rvmrc file?")
           @ruby_version = ask("What is your current ruby version (default: 1.9.3-p194)")
           @ruby_version = "1.9.3-p194" if @ruby_version.blank?
-          template '../templates/rvmrc.erb', '.rvmrc'
+          template "../templates/rvmrc.erb", ".rvmrc"
           system("/bin/bash -ce '[[ -s \"$HOME/.rvm/scripts/rvm\" ]] && source \"$HOME/.rvm/scripts/rvm\" && rvm use #{@ruby_version}@#{Rails.application.class.parent_name} --create'")
         end
       end
 
       def install_test_env
         if yes?("Would you like to install a test Environment")
-          gem("rspec-rails", '~> 2.12.2', :group => "test")
-          gem("annotate", :group => "test")
-          gem("ruby-graphviz", :group => "test")
-          gem("mysql2", :group => "test")
-          gem("cucumber", :group => "test")
-          gem("cucumber-rails", :group => "test", :require => false)
-          gem("factory_girl", :group => "test")
-          gem("factory_girl_rails", :group => "test")
-          gem("database_cleaner", :group => "test")
-          gem("capybara", :group => "test")
-          gem("capybara-webkit", :group => "test")
-          gem("guard", :group => "test")
-          gem("guard-rspec", :group => "test")
-          gem("guard-cucumber", :group => "test")
-          gem("guard-livereload", :group => "test")
-          gem("rb-fsevent", '~> 0.9.1', :group => "test")
-          gem("growl", :group => "test")
-          gem("launchy", :group => "test")
-          gem("faker", :group => "test")
-          gem("email_spec", :group => "test")
-          gem("shoulda-matchers", :group => "test")
+          gem("rspec-rails", '~> 2.12.2', group: "test")
+          gem("annotate", group: "test")
+          gem("ruby-graphviz", group: "test")
+          gem("mysql2", group: "test")
+          gem("cucumber", group: "test")
+          gem("cucumber-rails", group: "test", require: false)
+          gem("factory_girl", group: "test")
+          gem("factory_girl_rails", group: "test")
+          gem("database_cleaner", group: "test")
+          gem("capybara", group: "test")
+          gem("capybara-webkit", group: "test")
+          gem("guard", group: "test")
+          gem("guard-rspec", group: "test")
+          gem("guard-cucumber", group: "test")
+          gem("guard-livereload", group: "test")
+          gem("rb-fsevent", '~> 0.9.1', group: "test")
+          gem("growl", group: "test")
+          gem("launchy", group: "test")
+          gem("faker", group: "test")
+          gem("email_spec", group: "test")
+          gem("shoulda-matchers", group: "test")
           system("bundle install")
         end
       end
 
       def modify_production_env
         line = "config.assets.compile = false"
-        gsub_file 'config/environments/production.rb', /(#{Regexp.escape(line)})/mi do |match|
+        gsub_file "config/environments/production.rb", /(#{Regexp.escape(line)})/mi do |match|
           "config.assets.compile = true"
         end
       end
@@ -60,38 +58,37 @@ module Goldencobra
         end
 
         line = "class Application < Rails::Application"
-        gsub_file 'config/environments/production.rb', /(#{Regexp.escape(line)})/mi do |match|
+        gsub_file "config/environments/production.rb", /(#{Regexp.escape(line)})/mi do |match|
           "class Application < Rails::Application"
           "  require 'pdfkit'"
-          "  config.middleware.use PDFKit::Middleware, :print_media_type => true"
+          "  config.middleware.use PDFKit::Middleware, print_media_type: true"
         end
-
       end
 
       def install_gems
-        gem('acts-as-taggable-on', :git => 'git://github.com/mbleigh/acts-as-taggable-on')
-        gem('meta-tags', :git => 'git://github.com/jazzgumpy/meta-tags.git')
-        #gem('compass-960-plugin')
-        #gem('progress_bar')
-        #gem('compass-rails')
-        #gem('mysql2')
+        gem("acts-as-taggable-on", git: "git://github.com/mbleigh/acts-as-taggable-on")
+        gem("meta-tags", git: "git://github.com/jazzgumpy/meta-tags.git")
+        gem("bootstrap-sass")
+        gem("font-awesome-sass")
+        gem("autoprefixer-rails")
         system("bundle install")
       end
 
       def copy_initializer
         @underscored_user_name = "user".underscore
-        template '../templates/active_admin.rb.erb', 'config/initializers/active_admin.rb'
+        template "../templates/active_admin.rb.erb", "config/initializers/active_admin.rb"
       end
 
       def install_assets
-        require 'rails'
-        require 'active_admin'
+        require "rails"
+        require "active_admin"
         remove_file "app/assets/stylesheets/application.css"
         directory "../templates/assets", "app/assets"
         directory "../templates/views", "app/views"
-        template '../templates/extend_goldencobra_articles_controller.rb', 'app/controllers/extend_goldencobra_articles_controller.rb'
+        template "../templates/extend_goldencobra_articles_controller.rb",
+                 "app/controllers/extend_goldencobra_articles_controller.rb"
         remove_file "public/index.html"
-        copy_file '../templates/robots.txt', 'public/robots.txt'
+        copy_file "../templates/robots.txt", "public/robots.txt"
       end
 
       def setup_routes
@@ -106,8 +103,8 @@ module Goldencobra
       end
 
       def install_optional_assets
-        gem("better_errors", :group => "development")
-        gem("binding_of_caller", :group => "development")
+        gem("better_errors", group: "development")
+        gem("binding_of_caller", group: "development")
         system("bundle install")
       end
 
@@ -122,7 +119,7 @@ module Goldencobra
 
           @port = ask("What is your Errbit Port? (bsp: 80 default | 443 secure )")
           @port = "80" if @port.blank?
-          template '../templates/errbit.rb.erb', 'config/initializers/errbit.rb'
+          template "../templates/errbit.rb.erb", "config/initializers/errbit.rb"
       end
 
       def install_newrelic
@@ -131,7 +128,7 @@ module Goldencobra
           system("bundle install")
           @license_key = ask("What is your NewRelic license key? (default: b199ad3e4e0d728b1aac69aec4870af7ef9478bb)")
           @license_key = "b199ad3e4e0d728b1aac69aec4870af7ef9478bb" if @license_key.blank?
-          template '../templates/newrelic.yml.erb', 'config/newrelic.yml'
+          template "../templates/newrelic.yml.erb", "config/newrelic.yml"
         end
       end
 
@@ -140,7 +137,7 @@ module Goldencobra
         @admin_email = "admin@goldencobra.de" if @admin_email.blank?
 
         @admin_password = ask("Please enter a new password for admin account (user: #{@admin_email}):")
-        template '../templates/seeds.rb.erb', "db/seeds.rb"
+        template "../templates/seeds.rb.erb", "db/seeds.rb"
         if yes?("Would you like to create your local db?")
           system("bundle install")
           rake("db:create")
@@ -158,10 +155,10 @@ module Goldencobra
         if yes?("Would you like to configure git?")
           @git_url = ask("What is your git url? (bsp: ssh://git@git.ikusei.de:7999/KLIMA/website.git)")
           git :init
-          git :remote => "add origin #{@git_url}"
-          git :add => "."
-          git :commit => "-m 'First commit'"
-          git :push => "origin master"
+          git remote: "add origin #{@git_url}"
+          git add: "."
+          git commit: "-m 'First commit'"
+          git push: "origin master"
         end
         if yes?("Would you like to configure capistrano? (a git repository is required)")
           @ip_address = ask("To which IP do you want to deploy? (bsp: Taurus 178.23.121.27)")
@@ -171,12 +168,12 @@ module Goldencobra
           @app_name = Rails.application.class.parent_name.parameterize.underscore
           capify!
           remove_file "config/deploy.rb"
-          template '../templates/deploy.rb.erb', 'config/deploy.rb'
+          template "../templates/deploy.rb.erb", "config/deploy.rb"
 
           #Add Changes to git
-          git :add => "."
-          git :commit => "-m 'Deploy files added'"
-          git :push => "origin master"
+          git add: "."
+          git commit: "-m 'Deploy files added'"
+          git push: "origin master"
         end
         if yes?("Would you like to configure your server and deploy to it?")
           copy_file '../templates/create_database.mysql.erb', 'config/templates/create_database.mysql.erb'
@@ -185,9 +182,9 @@ module Goldencobra
           system("bundle install")
 
           #Add Changes to git
-          git :add => "."
-          git :commit => "-m 'Server configuration files added'"
-          git :push => "origin master"
+          git add: "."
+          git commit: "-m 'Server configuration files added'"
+          git push: "origin master"
 
           system("cap deploy:create_gemset")
           system("cap deploy:setup")
