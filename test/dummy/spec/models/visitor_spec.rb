f3ecae93918eecd6758e7acf394dade02da3c431
Holger Frohloff <holger@5minutenpause.com>
2015-12-15 17:09:45 +0100

    Updates Rspec tests
    There is still one pending test that just does not work. The rest of the Rspecs work again.

--- test/dummy/spec/models/visitor_spec.rb
+++ test/dummy/spec/models/visitor_spec.rb
@@ -5,6 +5,7 @@ require 'spec_helper'
 # has_many :role_users, :as => :operator
 # has_many :roles, :through => :role_users
 
-describe Visitor do
-  it { should have_many(:roles).through(:role_users) }
+describe Visitor, type: :model do
+  it { should have_many(:role_users).class_name("Goldencobra::RoleUser") }
+  it { should have_many(:roles).through(:role_users).class_name("Goldencobra::Role") }
 end
