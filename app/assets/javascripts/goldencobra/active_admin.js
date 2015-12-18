2b8a0163580eb14c8cb7a0f1729e640b8385a750
Marco Metz <metz@ikusei.de>
2015-12-18 17:01:03 +0100

    Mehrer Bugs beseitigt bei Goldencobra::Images
    - Löschen der Abhängigkeiten von Bildern funktioniert nun
    - ReactAPI Response leiferte ein falsches JSOn zurück

--- app/assets/javascripts/goldencobra/active_admin.js
+++ app/assets/javascripts/goldencobra/active_admin.js
@@ -276,8 +276,9 @@ function populateArticleUploads() {
         var selectedUploadId = $(element).find('option:selected').val();
 
         $this.outerHTML = "<div id='react-" + thisId + "'></div>";
+        console.log(thisId, selectedUploadId, thisData, thisName, includeBlank);
         React.render(
-          React.createElement(SelectList, {id: thisId, value: selectedUploadId, options: thisData, name: thisName, firstBlank: includeBlank}),
+          React.createElement(SelectList, { id: thisId, value: selectedUploadId, options: thisData, name: thisName, firstBlank: includeBlank }),
           document.getElementById('react-' + thisId)
         );
 
