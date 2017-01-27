c89c31bafdec304ed45420c08dfb95bdd575520c
Holger Frohloff <h.frohloff@ikusei.de>
2017-01-27 17:33:58 +0100

    Wrap at 100 cols

--- admin/uploads.rb
+++ admin/uploads.rb
@@ -5,8 +5,11 @@ ActiveAdmin.register Goldencobra::Upload, as: "Upload" do
 
   if ActiveRecord::Base.connection.table_exists?("tags")
     Goldencobra::Upload.tag_counts_on(:tags).each do |utag|
-      if(utag.count > 5)
-        scope(I18n.t(utag.name, scope: [:goldencobra, :widget_types], default: utag.name), show_count: false) { |t| t.tagged_with(utag.name) }
+      if utag.count > 5
+        scope(
+          I18n.t(utag.name, scope: [:goldencobra, :widget_types], default: utag.name),
+          show_count: false
+        ) { |t| t.tagged_with(utag.name) }
       end
     end
   end
