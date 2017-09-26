67d4706b598bbca62496b16c88359349751ea3b8
Anna Ronacher <a.ronacher@ikusei.de>
2017-09-26 15:58:50 +0200

    added gsub with whitelist for filename
    added gsub with whitelist for filename

--- test/dummy/spec/models/upload_spec.rb
+++ test/dummy/spec/models/upload_spec.rb
@@ -3,30 +3,29 @@
 require "spec_helper"
 
 describe Goldencobra::Upload do
-  it "can upload a zip file" do
-     zip = Goldencobra::Upload.create(image: File.open(File.join(Rails.root, "spec/fixtures/files/test_archive.zip")))
-     zip.unzip_files
+  before(:all) do
+    @zip = Goldencobra::Upload.create(image: File.open(File.join(Rails.root, "spec/fixtures/files/test_archive.zip")))
+    @zip_severals = Goldencobra::Upload.create(image: File.open(File.join(Rails.root, "spec/fixtures/files/test_archive_severals.zip")))
+    @zip_subfolders = Goldencobra::Upload.create(image: File.open(File.join(Rails.root, "spec/fixtures/files/test_archive_subfolders.zip")))
+  end
+
+  it "can upload and unzip a zip file" do
+     @zip.unzip_files
   end
 
   it "creates an Goldencobra::Upload for each file contained" do
-    zip = Goldencobra::Upload.create(image: File.open(File.join(Rails.root, "spec/fixtures/files/test_archive.zip")))
-    zip2 = Goldencobra::Upload.create(image: File.open(File.join(Rails.root, "spec/fixtures/files/test_archive_severals.zip")))
-
-    expect { zip.unzip_files }.to change(Goldencobra::Upload, :count).by(1)
-    expect { zip2.unzip_files }.to change(Goldencobra::Upload, :count).by(3)
+    expect { @zip.unzip_files }.to change(Goldencobra::Upload, :count).by(1)
+    expect { @zip_severals.unzip_files }.to change(Goldencobra::Upload, :count).by(3)
   end
 
   it "keeps the original name of the file" do
-    zip = Goldencobra::Upload.create(image: File.open(File.join(Rails.root, "spec/fixtures/files/test_archive.zip")))
-    zip.unzip_files
+    @zip.unzip_files
     last_record = Goldencobra::Upload.last.image_file_name
 
     expect(last_record).to eq("test.png")
   end
 
   it "also extracts images in subfolders" do
-    zip = Goldencobra::Upload.create(image: File.open(File.join(Rails.root, "spec/fixtures/files/test_archive_subfolders.zip")))
-
-    expect { zip.unzip_files }.to change(Goldencobra::Upload, :count).by(5)
+    expect { @zip_subfolders.unzip_files }.to change(Goldencobra::Upload, :count).by(5)
   end
 end
