c86319c92243b1822640a814738c1456bcd1099d
Anna Ronacher <a.ronacher@ikusei.de>
2017-09-26 10:28:53 +0200

    also works with dots and spaces in filename
     files with ._ are now skipped

--- app/models/goldencobra/upload.rb
+++ app/models/goldencobra/upload.rb
@@ -97,47 +97,22 @@ module Goldencobra
       if self.image_file_name.include?(".zip") && File.exists?(self.image.path)
         require 'zip'
         zipped_files = Zip::File.open(self.image.path)
-        int = 0
         zipped_files.each do |zipped_file|
-          int = int + 1
-
-          filename = zipped_file.name.split(".").last(2).join(".")
-
+          filename = zipped_file.name.split('/').last.gsub(" ", "_")
+          next if filename[0] == "."
           if zipped_file.file?
-            zipped_file.extract("tmp/#{filename}")
-            Goldencobra::Upload.create(image: File.open("tmp/#{filename}"),
+            zipped_file.extract("tmp/#{self.id}_#{filename}")
+            Goldencobra::Upload.create(image: File.open("tmp/#{self.id}_#{filename}"),
+                        image_file_name: filename,
                         source: self.source,
                         rights: self.rights,
                         description: self.description,
                         tag_list: self.tag_list.join(", ") )
-            File.delete("tmp/#{filename}")
+            File.delete("tmp/#{self.id}_#{filename}")
           end
         end
       end
     end
-
-    # def unzip_files
-    #   if self.image_file_name.include?(".zip") && File.exists?(self.image.path)
-    #     require 'zip'
-    #     zipped_files = Zip::File.open(self.image.path)
-    #     int = 0
-    #     zipped_files.each do |zipped_file|
-    #       int = int + 1
-    #       if zipped_file.file?
-    #         file_name = zipped_file.image_file_name
-    #         file_ending = zipped_file.extname
-    #         zipped_file.extract("tmp/#{orig_name}.#{file_ending}")
-    #         Goldencobra::Upload.create(image: File.open("tmp/#{orig_name}.#{file_ending}"),
-    #                     image_file_name: "#{orig_name}.#{file_ending}",
-    #                     source: self.source,
-    #                     rights: self.rights,
-    #                     description: self.description,
-    #                     tag_list: self.tag_list.join(", ") )
-    #         File.delete("tmp/#{orig_name}.#{file_ending}")
-    #       end
-    #     end
-    #   end
-    # end
 
     # Internal: Makes sure to only post-process files which are either pdf
     # or image files. Word documents would break file upload.
